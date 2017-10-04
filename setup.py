56d7292f1d439bfa1cafa34fd2b34b8351d3b491
Scott Lundberg <slund1@Scotts-MBP.T-mobile.com>
2017-10-04 12:06:00 -0700

    Focus this package on python

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 from setuptools import setup
 
+# to publish use:
+# > python setup.py sdist upload
+# which depends on ~/.pypirc
 
 setup(name='shap',
-      version='0.3.3',
+      version='0.4',
       description='Explains the output of any machine learning model using expectations and Shapley values.',
       url='http://github.com/slundberg/shap',
       author='Scott Lundberg',
       author_email='slund1@cs.washington.edu',
       license='MIT',
       packages=['shap'],
-      install_requires=['numpy', 'scipy', 'iml', 'sklearn'],
+      install_requires=['numpy', 'scipy', 'iml', 'sklearn', 'matplotlib'],
       test_suite='nose.collector',
       tests_require=['nose'],
       zip_safe=False)
--- src/python/pypi_upload.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-
-# depends on ~/.pypirc
-python setup.py sdist upload #-r https://pypi.python.org/pypi
