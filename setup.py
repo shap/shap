c3663a7f9ae9190dbd7ba18c0ce85349a3ca7fc8
Scott Lundberg <slund1@cs.washington.edu>
2018-04-10 12:29:16 -0700

    Fix setup.py

--- setup.py
+++ setup.py
@@ -5,16 +5,18 @@ import numpy
 # > python setup.py sdist upload
 # which depends on ~/.pypirc
 
-setup(name='shap',
-      version='0.13.1',
-      description='A unified approach to explain the output of any machine learning model.',
-      url='http://github.com/slundberg/shap',
-      author='Scott Lundberg',
-      author_email='slund1@cs.washington.edu',
-      license='MIT',
-      packages=['shap'],
-      install_requires=['numpy', 'scipy', 'iml>=0.5.1', 'scikit-learn', 'matplotlib', 'pandas', 'tqdm'],
-      test_suite='nose.collector',
-      tests_require=['nose', 'xgboost'],
-      #ext_modules = [Extension('shap._ctreeshap', sources=['shap/_ctreeshap.cc'], include_dirs=[numpy.get_include()])],
-      zip_safe=False)
+setup(
+    name='shap',
+    version='0.13.2',
+    description='A unified approach to explain the output of any machine learning model.',
+    url='http://github.com/slundberg/shap',
+    author='Scott Lundberg',
+    author_email='slund1@cs.washington.edu',
+    license='MIT',
+    packages=['shap', 'shap.explainers'],
+    install_requires=['numpy', 'scipy', 'iml>=0.5.1', 'scikit-learn', 'matplotlib', 'pandas', 'tqdm'],
+    test_suite='nose.collector',
+    tests_require=['nose', 'xgboost'],
+    ext_modules = [Extension('shap._cext', sources=['shap/_cext.cc'], include_dirs=[numpy.get_include()])],
+    zip_safe=False
+)
