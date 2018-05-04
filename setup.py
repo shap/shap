204b9e58a156e4f0faf7a240b5923a7c0f305c09
Oliver Laslett <o.laslett@soton.ac.uk>
2018-05-04 15:18:06 +0100

    update setup.py to bootstrap numpy installation

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
 from setuptools import setup, Extension
-import numpy
+from setuptools.command.build_ext import build_ext as _build_ext
 
 # to publish use:
 # > python setup.py sdist upload
 # which depends on ~/.pypirc
+
+# Extend the default build_ext class to bootstrap numpy installation
+# that are needed to build C extensions.
+# see https://stackoverflow.com/questions/19919905/how-to-bootstrap-numpy-installation-in-setup-py
+class build_ext(_build_ext):
+    def finialize_options(self):
+        _build_ext.finalize_options(self)
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
+
 
 def run_setup(with_binary):
     ext_modules = []
@@ -21,6 +32,8 @@ def run_setup(with_binary):
         author_email='slund1@cs.washington.edu',
         license='MIT',
         packages=['shap', 'shap.explainers'],
+        cmdclass={'build_ext': build_ext},
+        setup_requires=['numpy'],
         install_requires=['numpy', 'scipy', 'iml>=0.6.0', 'scikit-learn', 'matplotlib', 'pandas', 'tqdm'],
         test_suite='nose.collector',
         tests_require=['nose', 'xgboost'],
