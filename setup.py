a941568cebffc2a130df699a5d8280c0b0f04f48
Scott Lundberg <slundberg@users.noreply.github.com>
2018-05-30 13:25:58 -0700

    Print exception during failed C extension

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ def run_setup(with_binary):
 
 try:
     run_setup(True)
-except:
+except e:
+    print(e)
     print("WARNING: The C extension could not be compiled, sklearn tree models not supported.")
     run_setup(False)
