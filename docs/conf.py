52ac94818ab87d46265030270ceb9084e55e2b84
Scott Lundberg <slund1@cs.washington.edu>
2018-07-07 10:24:57 -0700

    Update docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,6 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import os
-os.system("pip install shap")
 import sys
 print(os.path.abspath('./shap'))
 sys.path.insert(0, os.path.abspath('..'))
@@ -126,7 +125,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
