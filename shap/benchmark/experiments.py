1b0e50549cbb23254e5c89eca05f1862fc9f14da
Scott Lundberg <slund1@cs.washington.edu>
2018-09-01 09:23:00 -0700

    Fix Queue import for py27

--- shap/benchmark/experiments.py
+++ shap/benchmark/experiments.py
@@ -15,7 +15,10 @@ from multiprocessing import Pool
 import itertools
 import copy
 import random
-from queue import Queue
+try:
+    from queue import Queue
+except ImportError:
+    from Queue import Queue
 from threading import Thread
 
 
