76eb3adb69e843824de3f820329472dc235e6eb2
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2019-01-03 08:35:49 -0500

    Add interim layer tests for pytorch explainers

--- tests/explainers/test_deep.py
+++ tests/explainers/test_deep.py
@@ -149,6 +149,78 @@ def test_pytorch_mnist_cnn():
         return
     import shap
 
+    def run_test(train_loader, test_loader, interim):
+
+        class Net(nn.Module):
+            def __init__(self):
+                super(Net, self).__init__()
+                # Testing several different activations
+                self.conv_layers = nn.Sequential(
+                    nn.Conv2d(1, 10, kernel_size=5),
+                    nn.MaxPool2d(2),
+                    nn.Tanh(),
+                    nn.Conv2d(10, 20, kernel_size=5),
+                    nn.MaxPool2d(2),
+                    nn.Softplus(),
+                )
+                self.fc_layers = nn.Sequential(
+                    nn.Linear(320, 50),
+                    nn.ReLU(),
+                    nn.Linear(50, 10),
+                    nn.ELU(),
+                    nn.Softmax(dim=1)
+                )
+
+            def forward(self, x):
+                x = self.conv_layers(x)
+                x = x.view(-1, 320)
+                x = self.fc_layers(x)
+                return x
+
+        model = Net()
+        optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+
+        def train(model, device, train_loader, optimizer, epoch, cutoff=2000):
+            model.train()
+            num_examples = 0
+            for batch_idx, (data, target) in enumerate(train_loader):
+                num_examples += target.shape[0]
+                data, target = data.to(device), target.to(device)
+                optimizer.zero_grad()
+                output = model(data)
+                loss = F.mse_loss(output, torch.eye(10)[target])
+                # loss = F.nll_loss(output, target)
+                loss.backward()
+                optimizer.step()
+                if batch_idx % 10 == 0:
+                    print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                        epoch, batch_idx * len(data), len(train_loader.dataset),
+                               100. * batch_idx / len(train_loader), loss.item()))
+                if num_examples > cutoff:
+                    break
+
+        device = torch.device('cpu')
+        train(model, device, train_loader, optimizer, 1)
+
+        next_x, next_y = next(iter(train_loader))
+        np.random.seed(0)
+        inds = np.random.choice(next_x.shape[0], 20, replace=False)
+        if interim:
+            e = shap.DeepExplainer((model, model.conv_layers[0]), next_x[inds, :, :, :])
+        else:
+            e = shap.DeepExplainer(model, next_x[inds, :, :, :])
+        test_x, test_y = next(iter(test_loader))
+        shap_values = e.shap_values(test_x[:1])
+
+        model.eval()
+        model.zero_grad()
+        with torch.no_grad():
+            diff = (model(test_x[:1]) - model(next_x[inds, :, :, :])).detach().numpy().mean(0)
+        sums = np.array([shap_values[i].sum() for i in range(len(shap_values))])
+        d = np.abs(sums - diff).sum()
+        assert d / np.abs(diff).sum() < 0.001, "Sum of SHAP values does not match difference! %f" % (
+                d / np.abs(diff).sum())
+
     batch_size = 128
     root_dir = 'mnist_data'
 
@@ -167,73 +239,10 @@ def test_pytorch_mnist_cnn():
                        ])),
         batch_size=batch_size, shuffle=True)
 
-    class Net(nn.Module):
-        def __init__(self):
-            super(Net, self).__init__()
-            # Testing several different activations
-            self.conv_layers = nn.Sequential(
-                nn.Conv2d(1, 10, kernel_size=5),
-                nn.MaxPool2d(2),
-                nn.Tanh(),
-                nn.Conv2d(10, 20, kernel_size=5),
-                nn.MaxPool2d(2),
-                nn.Softplus(),
-            )
-            self.fc_layers = nn.Sequential(
-                nn.Linear(320, 50),
-                nn.ReLU(),
-                nn.Linear(50, 10),
-                nn.ELU(),
-                nn.Softmax(dim=1)
-            )
-
-        def forward(self, x):
-            x = self.conv_layers(x)
-            x = x.view(-1, 320)
-            x = self.fc_layers(x)
-            return x
-
-    model = Net()
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
-
-    def train(model, device, train_loader, optimizer, epoch, cutoff=2000):
-        model.train()
-        num_examples = 0
-        for batch_idx, (data, target) in enumerate(train_loader):
-            num_examples += target.shape[0]
-            data, target = data.to(device), target.to(device)
-            optimizer.zero_grad()
-            output = model(data)
-            loss = F.mse_loss(output, torch.eye(10)[target])
-            # loss = F.nll_loss(output, target)
-            loss.backward()
-            optimizer.step()
-            if batch_idx % 10 == 0:
-                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                    epoch, batch_idx * len(data), len(train_loader.dataset),
-                           100. * batch_idx / len(train_loader), loss.item()))
-            if num_examples > cutoff:
-                break
-
-    device = torch.device('cpu')
-    train(model, device, train_loader, optimizer, 1)
-
-    next_x, next_y = next(iter(train_loader))
-    np.random.seed(0)
-    inds = np.random.choice(next_x.shape[0], 20, replace=False)
-    e = shap.DeepExplainer(model, next_x[inds, :, :, :])
-    test_x, test_y = next(iter(test_loader))
-    shap_values = e.shap_values(test_x[:1])
-
-    model.eval()
-    model.zero_grad()
-    with torch.no_grad():
-        diff = (model(test_x[:1]) - model(next_x[inds, :, :, :])).detach().numpy().mean(0)
-    sums = np.array([shap_values[i].sum() for i in range(len(shap_values))])
-    d = np.abs(sums - diff).sum()
-    assert d / np.abs(diff).sum() < 0.001, "Sum of SHAP values does not match difference! %f" % (
-            d / np.abs(diff).sum())
-
+    print ('Running test on interim layer')
+    run_test(train_loader, test_loader, interim=True)
+    print ('Running test on whole model')
+    run_test(train_loader, test_loader, interim=False)
     # clean up
     shutil.rmtree(root_dir)
 
