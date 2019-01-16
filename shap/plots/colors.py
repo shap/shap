981c59868f260acdbfffb773eb1f79b8556e2565
Scott Lundberg <slund1@cs.washington.edu>
2019-01-15 21:15:19 -0800

    Re-include colors needed by image_plot

--- shap/plots/colors.py
+++ shap/plots/colors.py
@@ -90,22 +90,22 @@ try:
         "alpha": alphas
     })
 
-    # colors = []
-    # for l in np.linspace(1, 0, 100):
-    #     colors.append((30./255, 136./255, 229./255,l))
-    # for l in np.linspace(0, 1, 100):
-    #     colors.append((255./255, 13./255, 87./255,l))
-    # red_transparent_blue = LinearSegmentedColormap.from_list("red_transparent_blue", colors)
+    colors = []
+    for l in np.linspace(1, 0, 100):
+        colors.append((30./255, 136./255, 229./255,l))
+    for l in np.linspace(0, 1, 100):
+        colors.append((255./255, 13./255, 87./255,l))
+    red_transparent_blue = LinearSegmentedColormap.from_list("red_transparent_blue", colors)
 
-    # colors = []
-    # for l in np.linspace(0, 1, 100):
-    #     colors.append((30./255, 136./255, 229./255,l))
-    # transparent_blue = LinearSegmentedColormap.from_list("transparent_blue", colors)
+    colors = []
+    for l in np.linspace(0, 1, 100):
+        colors.append((30./255, 136./255, 229./255,l))
+    transparent_blue = LinearSegmentedColormap.from_list("transparent_blue", colors)
 
-    # colors = []
-    # for l in np.linspace(0, 1, 100):
-    #     colors.append((255./255, 13./255, 87./255,l))
-    # transparent_red = LinearSegmentedColormap.from_list("transparent_red", colors)
+    colors = []
+    for l in np.linspace(0, 1, 100):
+        colors.append((255./255, 13./255, 87./255,l))
+    transparent_red = LinearSegmentedColormap.from_list("transparent_red", colors)
 
 
 except ImportError:
