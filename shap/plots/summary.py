e376ab596482a9cd6cca19a69fd9d2c72d3eb204
xEcEz <fruiteam@gmail.com>
2019-05-17 18:01:36 +0200

    Reverts old formatting

--- shap/plots/summary.py
+++ shap/plots/summary.py
@@ -6,7 +6,6 @@ from __future__ import division
 import warnings
 import numpy as np
 from scipy.stats import gaussian_kde
-
 try:
     import matplotlib.pyplot as pl
 except ImportError:
@@ -15,12 +14,11 @@ except ImportError:
 from . import labels
 from . import colors
 
-
 # TODO: remove unused title argument / use title argument
 def summary_plot(shap_values, features=None, feature_names=None, max_display=None, plot_type="dot",
                  color=None, axis_color="#333333", title=None, alpha=1, show=True, sort=True,
                  color_bar=True, auto_size_plot=True, layered_violin_max_num_bins=20, class_names=None,
-                 export_path=None, width=10):
+                 export_path=None, width=8):
     """Create a SHAP summary plot, colored by feature values when they are provided.
 
     Parameters
@@ -44,7 +42,7 @@ def summary_plot(shap_values, features=None, feature_names=None, max_display=Non
     multi_class = False
     if isinstance(shap_values, list):
         multi_class = True
-        plot_type = "bar"  # only type supported for now
+        plot_type = "bar" # only type supported for now
     else:
         assert len(shap_values.shape) != 1, "Summary plots need a matrix of shap_values, not a vector."
 
@@ -53,7 +51,7 @@ def summary_plot(shap_values, features=None, feature_names=None, max_display=Non
         if plot_type == 'layered_violin':
             color = "coolwarm"
         elif multi_class:
-            color = lambda i: colors.red_blue_circle(i / len(shap_values))
+            color = lambda i: colors.red_blue_circle(i/len(shap_values))
         else:
             color = colors.blue_rgb
 
@@ -133,7 +131,7 @@ def summary_plot(shap_values, features=None, feature_names=None, max_display=Non
             pl.savefig(export_path)
         if show:
             pl.show()
-        return pl
+        return
 
     if max_display is None:
         max_display = 20
@@ -300,8 +298,7 @@ def summary_plot(shap_values, features=None, feature_names=None, max_display=Non
 
         # loop through each feature and plot:
         for pos, ind in enumerate(feature_order):
-            # decide how to handle: if #unique < layered_violin_max_num_bins then split by unique value,
-            # otherwise use bins/percentiles.
+            # decide how to handle: if #unique < layered_violin_max_num_bins then split by unique value, otherwise use bins/percentiles.
             # to keep simpler code, in the case of uniques, we just adjust the bins to align with the unique counts.
             feature = features[:, ind]
             unique, counts = np.unique(feature, return_counts=True)
@@ -327,8 +324,7 @@ def summary_plot(shap_values, features=None, feature_names=None, max_display=Non
                         "not enough data in bin #%d for feature %s, so it'll be ignored. Try increasing the number of "
                         "records to plot."
                         % (i, feature_names[ind]))
-                    # to ignore it, just set it to the previous y-values (so the area between them will be zero). Not
-                    #  ys is already 0, so there's
+                    # to ignore it, just set it to the previous y-values (so the area between them will be zero). Not ys is already 0, so there's
                     # nothing to do if i == 0
                     if i > 0:
                         ys[i, :] = ys[i - 1, :]
@@ -336,8 +332,7 @@ def summary_plot(shap_values, features=None, feature_names=None, max_display=Non
                 # save kde of them: note that we add a tiny bit of gaussian noise to avoid singular matrix errors
                 ys[i, :] = gaussian_kde(shaps + np.random.normal(loc=0, scale=0.001, size=shaps.shape[0]))(x_points)
                 # scale it up so that the 'size' of each y represents the size of the bin. For continuous data this will
-                # do nothing, but when we've gone with the unqique option, this will matter - e.g. if 99% are male
-                # and 1%
+                # do nothing, but when we've gone with the unqique option, this will matter - e.g. if 99% are male and 1%
                 # female, we want the 1% to appear a lot smaller.
                 size = thesebins[i + 1] - thesebins[i]
                 bin_size_if_even = features.shape[0] / nbins
@@ -352,8 +347,7 @@ def summary_plot(shap_values, features=None, feature_names=None, max_display=Non
             for i in range(nbins - 1, -1, -1):
                 y = ys[i, :] / scale
                 c = pl.get_cmap(color)(i / (
-                        nbins - 1)) if color in pl.cm.datad else color  # if color is a cmap, use it, otherwise use a
-                #  color
+                        nbins - 1)) if color in pl.cm.datad else color  # if color is a cmap, use it, otherwise use a color
                 pl.fill_between(x_points, pos - y, pos + y, facecolor=c)
         pl.xlim(shap_min, shap_max)
 
@@ -367,13 +361,13 @@ def summary_plot(shap_values, features=None, feature_names=None, max_display=Non
 
     elif multi_class and plot_type == "bar":
         if class_names is None:
-            class_names = ["Class " + str(i) for i in range(len(shap_values))]
+            class_names = ["Class "+str(i) for i in range(len(shap_values))]
         feature_inds = feature_order[:max_display]
         y_pos = np.arange(len(feature_inds))
         left_pos = np.zeros(len(feature_inds))
 
         class_inds = np.argsort([-np.abs(shap_values[i]).mean() for i in range(len(shap_values))])
-        for i, ind in enumerate(class_inds):
+        for i,ind in enumerate(class_inds):
             global_shap_values = np.abs(shap_values[ind]).mean(0)
             pl.barh(
                 y_pos, global_shap_values[feature_inds], 0.7, left=left_pos, align='center',
@@ -419,9 +413,6 @@ def summary_plot(shap_values, features=None, feature_names=None, max_display=Non
         pl.savefig(export_path)
     if show:
         pl.show()
-
-    return
-
 
 def shorten_text(text, length_limit):
     if len(text) > length_limit:
