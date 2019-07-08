0e2867d458af9dbaa96cae30cc189036725771da
Scott Lundberg <slundberg@users.noreply.github.com>
2019-07-08 10:26:28 -0700

    Update test_sampling.py

--- tests/explainers/test_sampling.py
+++ tests/explainers/test_sampling.py
@@ -5,14 +5,14 @@ import shap
 
 
 def test_null_model_small():
-    explainer = shap.SamplingExplainer(lambda x: np.zeros(x.shape[0]), np.ones((2, 4)), nsamples=100)
-    shap_values = explainer.shap_values(np.ones((1, 4)))
+    explainer = shap.SamplingExplainer(lambda x: np.zeros(x.shape[0]), np.ones((2, 4)))
+    shap_values = explainer.shap_values(np.ones((1, 4)), nsamples=100)
     assert np.sum(np.abs(shap_values)) < 1e-8
 
 
 def test_null_model():
-    explainer = shap.SamplingExplainer(lambda x: np.zeros(x.shape[0]), np.ones((2, 10)), nsamples=100)
-    shap_values = explainer.shap_values(np.ones((1, 10)))
+    explainer = shap.SamplingExplainer(lambda x: np.zeros(x.shape[0]), np.ones((2, 10)))
+    shap_values = explainer.shap_values(np.ones((1, 10)), nsamples=100)
     assert np.sum(np.abs(shap_values)) < 1e-8
 
 
@@ -30,8 +30,8 @@ def test_front_page_model_agnostic():
     svm.fit(X_train, Y_train)
 
     # use Kernel SHAP to explain test set predictions
-    explainer = shap.SamplingExplainer(svm.predict_proba, X_train, nsamples=100)
-    shap_values = explainer.shap_values(X_test)
+    explainer = shap.SamplingExplainer(svm.predict_proba, X_train)
+    shap_values = explainer.shap_values(X_test, nsamples=100)
 
     # plot the SHAP values for the Setosa output of the first instance
     shap.force_plot(explainer.expected_value[0], shap_values[0][0, :], X_test.iloc[0, :])
