fix the value calculation, from approx to accurate one. add stronger test

shap/explainers/tree.py
@@ -428,7 +428,7 @@ class TreeEnsemble:
             self.dtype = np.float32
             scaling = 1.0 / len(model.estimators_) # output is average of trees
             self.trees = [IsoTree(e.tree_, scaling=scaling, data=data, data_missing=data_missing) for e in model.estimators_]
-            self.objective = objective_name_map.get('mse', None)
+            self.objective = objective_name_map.get('mae', None)
             self.tree_output = "raw_value"
         elif str(type(model)).endswith("skopt.learning.forest.RandomForestRegressor'>"):
             self.dtype = np.float32
@@ -1016,12 +1016,15 @@ class IsoTree(Tree):
             from sklearn.ensemble.iforest import _average_path_length
 
             def _recalculate_value(tree, i , level):
-                value = level + _average_path_length(np.array([tree.n_node_samples[i]]))
-                self.values[i, 0] =  value
-                if tree.children_left[i] > 0 :
-                    _recalculate_value(tree, tree.children_left[i] , level + 1)
-                if tree.children_right[i] > 0 :
-                    _recalculate_value(tree, tree.children_right[i] , level + 1)
+                if tree.children_left[i] == -1 and tree.children_right[i] == -1:
+                    value = level + _average_path_length(np.array([tree.n_node_samples[i]]))[0]
+                    self.values[i, 0] =  value
+                    return value * tree.n_node_samples[i]
+                else:
+                    value_left = _recalculate_value(tree, tree.children_left[i] , level + 1)
+                    value_right = _recalculate_value(tree, tree.children_right[i] , level + 1)
+                    self.values[i, 0] =  (value_left + value_right) / tree.n_node_samples[i]
+                    return value_left + value_right
 
             _recalculate_value(tree, 0, 0)
             if normalize:

tests/explainers/test_tree.py
@@ -654,7 +654,13 @@ def test_isolation_forest():
 
     explainer = shap.TreeExplainer(iso)
     shap_values = explainer.shap_values(X)
-    assert np.allclose(explainer.expected_value, _average_path_length(np.array([iso.max_samples_]))[0])
+    assert np.allclose(explainer.expected_value, _average_path_length(np.array([iso.max_samples_]))[0], atol=1e-7)
+
+    score_from_shape = - 2**(
+        - (np.sum(shap_values, axis=1) + explainer.expected_value) /
+        _average_path_length(np.array([iso.max_samples_]))[0]
+        )
+    assert np.allclose(iso.score_samples(X), score_from_shape, atol=1e-7)
 
 def test_multi_target_extra_trees():
     import shap