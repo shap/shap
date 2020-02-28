1a28ea5923b03825671bfd8ff8b27108537673ca
rightx2 <rightx2@gmail.com>
2020-02-28 01:11:03 +0000

    Update fixtures: add _skip_if_no_pytorch()

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,5 +1,4 @@
 import random
-import torch
 import pytest
 
 import numpy as np
@@ -9,6 +8,10 @@ def set_random_seeds(seed_value=42):
     np.random.seed(seed_value)
     random.seed(seed_value)
 
+    from tests.explainers.test_deep import _skip_if_no_pytorch
+    _skip_if_no_pytorch()
+
+    import torch
     torch.manual_seed(seed_value)
     torch.cuda.manual_seed(seed_value)
     torch.cuda.manual_seed_all(seed_value)
