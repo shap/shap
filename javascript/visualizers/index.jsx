414cf32657222ee16a74f65ffe2c085c08cd1c2f
SachinVarghese <smv@seldon.io>
2020-04-10 03:42:53 +0100

    code changes to publish shapjs to npm

---
+++ javascript/visualizers/index.jsx
@@ -0,0 +1,15 @@
+import SimpleListVisualizer from "./SimpleListVisualizer";
+import AdditiveForceVisualizer from "./AdditiveForceVisualizer";
+import AdditiveForceArrayVisualizer from "./AdditiveForceArrayVisualizer";
+
+export {
+  SimpleListVisualizer,
+  AdditiveForceVisualizer,
+  AdditiveForceArrayVisualizer
+};
+
+export default {
+  SimpleListVisualizer,
+  AdditiveForceVisualizer,
+  AdditiveForceArrayVisualizer
+};
