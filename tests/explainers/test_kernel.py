64168ca8aebb414139ac843dc50837e0dab8dd6c
Michael Johnson <michaelj@avianaglobal.com>
2020-06-24 23:50:03 -0700

    Formatting/Optimize imports

--- tests/explainers/test_kernel.py
+++ tests/explainers/test_kernel.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy as sp
+
 import shap
 
 
@@ -8,10 +9,12 @@ def test_null_model_small():
     e = explainer.explain(np.ones((1, 4)))
     assert np.sum(np.abs(e)) < 1e-8
 
+
 def test_null_model():
     explainer = shap.KernelExplainer(lambda x: np.zeros(x.shape[0]), np.ones((2, 10)), nsamples=100)
     e = explainer.explain(np.ones((1, 10)))
     assert np.sum(np.abs(e)) < 1e-8
+
 
 def test_front_page_model_agnostic():
     import sklearn
@@ -33,6 +36,7 @@ def test_front_page_model_agnostic():
     # plot the SHAP values for the Setosa output of the first instance
     shap.force_plot(explainer.expected_value[0], shap_values[0][0, :], X_test.iloc[0, :], link="logit")
 
+
 def test_front_page_model_agnostic_rank():
     import sklearn
     import shap
@@ -53,6 +57,7 @@ def test_front_page_model_agnostic_rank():
     # plot the SHAP values for the Setosa output of the first instance
     shap.force_plot(explainer.expected_value[0], shap_values[0][0, :], X_test.iloc[0, :], link="logit")
 
+
 def test_kernel_shap_with_dataframe():
     from sklearn.linear_model import LinearRegression
     import shap
@@ -72,12 +77,13 @@ def test_kernel_shap_with_dataframe():
     explainer = shap.KernelExplainer(linear_model.predict, df_X, keep_index=True)
     shap_values = explainer.shap_values(df_X)
 
+
 def test_kernel_shap_with_a1a_sparse_zero_background():
     from sklearn.model_selection import train_test_split
     from sklearn.linear_model import LinearRegression
     import shap
 
-    X, y = shap.datasets.a1a() # pylint: disable=unbalanced-tuple-unpacking
+    X, y = shap.datasets.a1a()  # pylint: disable=unbalanced-tuple-unpacking
     x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.01, random_state=0)
     linear_model = LinearRegression()
     linear_model.fit(x_train, y_train)
@@ -88,6 +94,7 @@ def test_kernel_shap_with_a1a_sparse_zero_background():
     explainer = shap.KernelExplainer(linear_model.predict, background)
     explainer.shap_values(x_test)
 
+
 def test_kernel_shap_with_a1a_sparse_nonzero_background():
     np.set_printoptions(threshold=100000)
     from sklearn.model_selection import train_test_split
@@ -96,7 +103,7 @@ def test_kernel_shap_with_a1a_sparse_nonzero_background():
     import shap
     np.random.seed(0)
 
-    X, y = shap.datasets.a1a() # pylint: disable=unbalanced-tuple-unpacking
+    X, y = shap.datasets.a1a()  # pylint: disable=unbalanced-tuple-unpacking
     x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.01, random_state=0)
     linear_model = LinearRegression()
     linear_model.fit(x_train, y_train)
@@ -114,7 +121,8 @@ def test_kernel_shap_with_a1a_sparse_nonzero_background():
     x_test_dense = x_test.toarray()
     shap_values_dense = explainer_dense.shap_values(x_test_dense)
     # Validate sparse and dense result is the same
-    assert(np.allclose(shap_values, shap_values_dense, rtol=1e-02, atol=1e-01))
+    assert (np.allclose(shap_values, shap_values_dense, rtol=1e-02, atol=1e-01))
+
 
 def test_kernel_shap_with_high_dim_sparse():
     # verifies we can run on very sparse data produced from feature hashing
@@ -130,13 +138,13 @@ def test_kernel_shap_with_high_dim_sparse():
     ]
     from sklearn.datasets import fetch_20newsgroups
     ngroups = fetch_20newsgroups(subset='train', categories=categories,
-                        shuffle=True, random_state=42,
-                        remove=remove)
+                                 shuffle=True, random_state=42,
+                                 remove=remove)
     x_train, x_test, y_train, y_validation = train_test_split(ngroups.data, ngroups.target,
-                                                                    test_size=0.01, random_state=42)
+                                                              test_size=0.01, random_state=42)
     from sklearn.feature_extraction.text import HashingVectorizer
     vectorizer = HashingVectorizer(stop_words='english', alternate_sign=False,
-                                n_features=2**16)
+                                   n_features=2 ** 16)
     x_train = vectorizer.transform(x_train)
     x_test = vectorizer.transform(x_test)
     # Fit a linear regression model
@@ -148,8 +156,8 @@ def test_kernel_shap_with_high_dim_sparse():
     explainer = shap.KernelExplainer(linear_model.predict, background)
     shap_values = explainer.shap_values(x_test)
 
+
 def test_kernel_sparse_vs_dense_multirow_background():
-    import sklearn
     import shap
     from sklearn.model_selection import train_test_split
     from sklearn.linear_model import LogisticRegression
@@ -170,14 +178,15 @@ def test_kernel_sparse_vs_dense_multirow_background():
     lr_sparse.fit(X_sparse_train, Y_train)
 
     # use Kernel SHAP again but with sparse data
-    sparse_explainer = shap.KernelExplainer(lr.predict_proba, X_sparse_train, nsamples=100, link="logit", l1_reg="rank(3)")
+    sparse_explainer = shap.KernelExplainer(lr.predict_proba, X_sparse_train, nsamples=100, link="logit",
+                                            l1_reg="rank(3)")
     sparse_shap_values = sparse_explainer.shap_values(X_sparse_test)
 
-    assert(np.allclose(shap_values, sparse_shap_values, rtol=1e-05, atol=1e-05))
+    assert (np.allclose(shap_values, sparse_shap_values, rtol=1e-05, atol=1e-05))
 
     # Use sparse evaluation examples with dense background
     sparse_sv_dense_bg = explainer.shap_values(X_sparse_test)
-    assert(np.allclose(shap_values, sparse_sv_dense_bg, rtol=1e-05, atol=1e-05))
+    assert (np.allclose(shap_values, sparse_sv_dense_bg, rtol=1e-05, atol=1e-05))
 
 
 def test_linear():
@@ -189,7 +198,7 @@ def test_linear():
 
     # a linear model
     def f(x):
-        return x[:, 0] + 2.0*x[:, 1]
+        return x[:, 0] + 2.0 * x[:, 1]
 
     phi = shap.KernelExplainer(f, x).shap_values(x, l1_reg="num_features(2)", silent=True)
     assert phi.shape == x.shape
