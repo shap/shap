bb238dc6cf6bdd927bdf57345f32f4fe5c42229e
Scott Lundberg <scott.lundberg@microsoft.com>
2020-07-22 09:57:18 -0700

    Add slicer dependency

--- setup.py
+++ setup.py
@@ -104,7 +104,7 @@ def run_setup(with_binary=True, test_xgboost=True, test_lightgbm=True, test_catb
         package_data={'shap': ['plots/resources/*', 'tree_shap.h']},
         cmdclass={'build_ext': build_ext},
         setup_requires=['numpy'],
-        install_requires=['numpy', 'scipy', 'scikit-learn', 'pandas', 'tqdm>4.25.0'],
+        install_requires=['numpy', 'scipy', 'scikit-learn', 'pandas', 'tqdm>4.25.0', 'slicer'],
         extras_require=extras_require,
         test_suite='nose.collector',
         tests_require=tests_require,
