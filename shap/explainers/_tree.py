ca625c7c4ba83ba746d9cf07b1ec92c6b348725a
Diarmaid Finnerty <diarmaidfinnerty@gmail.com>
2020-10-24 20:18:10 +0100

    fix: Changed Tree to SingleTree in Adaboost

--- shap/explainers/_tree.py
+++ shap/explainers/_tree.py
@@ -748,7 +748,7 @@ class TreeEnsemble:
             self.internal_dtype = model.estimators_[0].tree_.value.dtype.type
             self.input_dtype = np.float32
             scaling = 1.0 / len(model.estimators_) # output is average of trees
-            self.trees = [Tree(e.tree_, normalize=True, scaling=scaling) for e in model.estimators_]
+            self.trees = [SingleTree(e.tree_, normalize=True, scaling=scaling) for e in model.estimators_]
             self.objective = objective_name_map.get(model.base_estimator_.criterion, None) #This line is done to get the decision criteria, for example gini.
             self.tree_output = "probability" #This is the last line added
         
