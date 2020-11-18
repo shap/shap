d2607e2400261d0148be9f48e43a2613d0c59afc
ryserrao <serrao.ryan1995@gmail.com>
2020-11-18 18:15:55 -0500

    Added fixed composite masker which returns (masked_x, x)

--- shap/maskers/_fixed_composite.py
+++ shap/maskers/_fixed_composite.py
@@ -1,5 +1,5 @@
-from ._masker import Masker
 import numpy as np
+from ._masker import Masker
 
 
 class FixedComposite(Masker):
@@ -8,12 +8,5 @@ class FixedComposite(Masker):
     
     def __call__(self, mask, x):
         masked_x = self.masker(mask, x)
-        if isinstance(x,str):
-            x = np.array([x])
-        else:
-            x = x.reshape(1, *x.shape)
-
-        out = np.append(masked_x, x, axis=0)
-        out = np.expand_dims(out, axis=0)
+        return masked_x, x
         
-        return out
