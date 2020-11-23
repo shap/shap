bc06a455057b8dd68d8a4d4dbb14b9bebe4deea2
ryserrao <serrao.ryan1995@gmail.com>
2020-11-23 14:44:30 -0500

    Updated FixedComposite masker to be create using a factory method to accommodate for dynamic inheritance

--- shap/maskers/_fixed_composite.py
+++ shap/maskers/_fixed_composite.py
@@ -1,11 +1,13 @@
 import numpy as np
 from ._masker import Masker
 
+def createFixedCompositeClass(cls, *args, **kwargs):
+    class FixedComposite(cls):
+        def __init__(self, *args, **kwargs):
+            super(FixedComposite, self).__init__(*args, **kwargs)
 
-class FixedComposite(Masker):
-    def __init__(self, masker):
-        self.masker = masker
-    
-    def __call__(self, mask, X):
-        masked_X = self.masker(mask, X)
-        return masked_X, X
+        def __call__(self, mask, *args):
+            masked_X = super(FixedComposite, self).__call__(mask, *args)
+            return masked_X
+
+    return FixedComposite(*args, **kwargs)
