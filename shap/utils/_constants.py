7ec487144728fc1283d85f13f5bcd4dc890c4696
ryserrao <serrao.ryan1995@gmail.com>
2020-11-23 07:52:11 -0500

    Added constants for seq2seq and causal lm model classes

---
+++ shap/utils/_constants.py
@@ -0,0 +1,32 @@
+
+
+MODELS_FOR_SEQ_TO_SEQ_CAUSAL_LM = [
+        "transformers.T5ForConditionalGeneration",
+        "transformers.PegasusForConditionalGeneration",
+        "transformers.MarianMTModel",
+        "transformers.MBartForConditionalGeneration",
+        "transformers.BlenderbotForConditionalGeneration",
+        "transformers.BartForConditionalGeneration",
+        "transformers.FSMTForConditionalGeneration",
+        "transformers.EncoderDecoderModel",
+        "transformers.XLMProphetNetForConditionalGeneration",
+        "transformers.ProphetNetForConditionalGeneration",
+    ]
+
+MODELS_FOR_CAUSAL_LM = [
+        "transformers.CamembertForCausalLM",
+        "transformers.XLMRobertaForCausalLM",
+        "transformers.RobertaForCausalLM",
+        "transformers.BertLMHeadModel",
+        "transformers.OpenAIGPTLMHeadModel",
+        "transformers.GPT2LMHeadModel",
+        "transformers.TransfoXLLMHeadModel",
+        "transformers.XLNetLMHeadModel",
+        "transformers.XLMWithLMHeadModel",
+        "transformers.CTRLLMHeadModel",
+        "transformers.ReformerModelWithLMHead",
+        "transformers.BertGenerationDecoder",
+        "transformers.XLMProphetNetForCausalLM",
+        "transformers.ProphetNetForCausalLM",
+    ]
+
