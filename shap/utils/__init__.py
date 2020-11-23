9ecbc184fe86d5788b996b4edc59e4e24d9f4e31
ryserrao <serrao.ryan1995@gmail.com>
2020-11-23 15:20:04 -0500

    Updated tokenizer parsing utility function

--- shap/utils/__init__.py
+++ shap/utils/__init__.py
@@ -1,6 +1,6 @@
 from ._clustering import hclust_ordering, partition_tree, partition_tree_shuffle, delta_minimization_order, hclust
 from ._general import approximate_interactions, potential_interactions, sample, safe_isinstance, assert_import, record_import_error
-from ._general import shapley_coefficients, convert_name, format_value, ordinal_str, OpChain, get_tokenizer_prefix_suffix
+from ._general import shapley_coefficients, convert_name, format_value, ordinal_str, OpChain, parse_prefix_suffix_for_tokenizer
 from ._show_progress import show_progress
 from ._masked_model import MaskedModel, make_masks
 from ._constants import MODELS_FOR_CAUSAL_LM, MODELS_FOR_SEQ_TO_SEQ_CAUSAL_LM
