58d722daf2348148e83342cd61e24b0c53b6bfe6
ryserrao <serrao.ryan1995@gmail.com>
2020-11-23 06:02:45 -0500

    added call function to generate target sentence ids and also to_device() to load variables to cuda/cpu

--- shap/models/_text_generation.py
+++ shap/models/_text_generation.py
@@ -12,3 +12,45 @@ class TextGeneration(Model):
             self.model_agnostic = False
         else:
             self.model_agnostic = True
+
+    def __call__(self, X):
+        target_sentence_ids = None
+        if self.model_agnostic:
+            target_sentence = self.model(X)
+            keep_prefix, keep_suffix = get_tokenizer_prefix_suffix(self.text_similarity_tokenizer)
+            if keep_suffix > 0:
+                target_sentence_ids = torch.tensor([self.text_similarity_tokenizer.encode(target_sentence)])[:,keep_prefix:-keep_suffix]
+            else:
+                target_sentence_ids = torch.tensor([self.text_similarity_tokenizer.encode(target_sentence)])[:,keep_prefix:]
+        else:
+            self.model.eval()
+            self.model = self.to_device(self.model, device=self.device)
+            input_ids = torch.tensor([self.tokenizer.encode(X)])
+            input_ids = self.to_device(input_ids, device=self.device)
+            with torch.no_grad():
+                output = self.model.generate(input_ids)
+            if (hasattr(self.model.config, "is_encoder_decoder") and not self.model.config.is_encoder_decoder) \
+                and (hasattr(self.model.config, "is_decoder") and not self.model.config.is_decoder):
+                raise ValueError(
+                    "Please assign either of is_encoder_decoder or is_decoder to True in model config for extracting target sentence ids"
+                )
+            if self.model.config.is_encoder_decoder:
+                keep_prefix, keep_suffix = get_tokenizer_prefix_suffix(self.tokenizer)
+                if keep_suffix > 0:
+                    target_sentence_ids = output[:, keep_prefix:-keep_suffix]
+                else:
+                    target_sentence_ids = output[:, keep_prefix:]
+            else:
+                # incase of only decoder we slice target ids after the input ids
+                target_sentence_ids = output[:,input_ids.shape[1]:]
+
+        return target_sentence_ids
+
+    def to_device(self, variables, device=None):
+        if isinstance(variables, list):
+            deviced_variables = []
+            for variable in variables:
+                deviced_variables.append(variable.to(device))
+            return deviced_variables
+        else:
+            return variables.to(device)
