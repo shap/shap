9b2917d1e3e124b7a6d7c7f63ad0ba52c989cd77
ryserrao <serrao.ryan1995@gmail.com>
2020-11-25 10:38:43 -0500

    Added fixed composite masker and updated __init__ to incorporate for its namespace

--- shap/maskers/_fixed_composite.py
+++ shap/maskers/_fixed_composite.py
@@ -1,16 +1,27 @@
 import numpy as np
 from ._masker import Masker
 
-def createFixedCompositeMasker(cls, *args, **kwargs):
-    class FixedComposite(cls):
-        def __init__(self, *args, **kwargs):
-            super(FixedComposite, self).__init__(*args, **kwargs)
+class FixedComposite(Masker):
+    def __init__(self, masker):
+        self.masker = masker
 
-        def __call__(self, mask, *args):
-            masked_X = super(FixedComposite, self).__call__(mask, *args)
-            wrapped_outputs = []
-            for masked_x in masked_X:
-                wrapped_outputs.append((masked_x,)+args)
-            return np.array(wrapped_outputs)
+    def __call__(self, mask, *args):
+        masked_X = self.masker(mask, *args)
+        return (masked_X,) + args
 
-    return FixedComposite(*args, **kwargs)
+    def shape(self, *args):
+        masker_rows , masker_cols = None, None
+        if hasattr(self.masker, "shape"):
+            if callable(self.masker.shape):
+                mshape = self.masker.shape(*args)
+                masker_rows = mshape[0]
+                masker_cols = mshape[1]
+            else:
+                mshape = self.masker.shape
+                masker_rows = mshape[0]
+                masker_cols = mshape[1]
+        else:
+            masker_rows = None# # just assuming...
+            masker_cols = sum(np.prod(a.shape) for a in args)
+
+        return masker_rows, masker_cols
