af47a1d00424a2e4f7b5e0a046b5b7232574d531
ryserrao <serrao.ryan1995@gmail.com>
2020-11-28 04:36:35 -0500

    Added comments

--- shap/explainers/_partition.py
+++ shap/explainers/_partition.py
@@ -101,7 +101,7 @@ class Partition(Explainer):
         else:
             self._reshaped_model = self.model
 
-        # if we don't have a dynamic clustering algorithm then can precowe mpute
+        # if we don't have a dynamic clustering algorithm then can precompute
         # a lot of information
         if not callable(self.masker.clustering):
             self._clustering = self.masker.clustering
@@ -136,6 +136,7 @@ class Partition(Explainer):
             except NameError:
                 prev_row_clustering = None
             self._clustering = self.masker.clustering(*row_args)
+            # to avoid recomputing masks for the same clustering
             if prev_row_clustering != self._clustering:
                 self._mask_matrix = make_masks(self._clustering)
 
