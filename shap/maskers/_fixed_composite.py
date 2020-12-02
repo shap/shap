2e2a5e78a0985c5c0593b876a071fe6e505a27df
ryserrao <serrao.ryan1995@gmail.com>
2020-12-02 15:23:23 -0500

    Added docstrings for _fixed_composite.py

--- shap/maskers/_fixed_composite.py
+++ shap/maskers/_fixed_composite.py
@@ -4,9 +4,23 @@ from ..utils import invariants, variants, shape, data_transform, clustering
 
 class FixedComposite(Masker):
     def __init__(self, masker):
+        """ Creates a Composite masker from an underlying masker and returns the original args along with the masked output.
+
+        Parameters
+        ----------
+        masker: object
+            An object of the shap.maskers.Masker base class (eg. Text/Image masker).
+
+        Returns
+        -------
+        list
+            A wrapped tuple consisting of the masked input using the underlying masker appended with the original args in a list.
+        """
         self.masker = masker
 
     def __call__(self, mask, *args):
+        """ Computes mask on the args using the masker data attribute and returns list having a wrapped tuple containing masked input with args.
+        """
         masked_X = self.masker(mask, *args)
         wrapped_args = []
         for item in args:
