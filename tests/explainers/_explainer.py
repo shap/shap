cff77862ae19757534a5be1c83f3b177dd9bdf2d
ryserrao <serrao.ryan1995@gmail.com>
2020-12-04 13:51:00 -0500

    Updated tests for _explainer.py

--- tests/explainers/_explainer.py
+++ tests/explainers/_explainer.py
@@ -1,19 +1,6 @@
 ''' Tests for Explainer class.
 '''
 
-def test_wrapping_for_text_to_text_pretrained_transformer_model():
-    import shap
-    from shap.utils import safe_isinstance
-    from transformers import AutoTokenizer, AutoModelForCausalLM
-    
-    tokenizer = AutoTokenizer.from_pretrained("gpt2")
-    model =  AutoModelForCausalLM.from_pretrained("gpt2")
-
-    explainer = shap.Explainer(model,tokenizer)
-
-    assert safe_isinstance(explainer.elemental_model, "shap.models.TeacherForcingLogits") \
-            and safe_isinstance(explainer.masker, "shap.maskers.FixedComposite")
-
 def test_wrapping_for_text_to_text_teacher_forcing_logits_model():
     from transformers import AutoTokenizer, AutoModelForCausalLM
     import shap
@@ -26,8 +13,8 @@ def test_wrapping_for_text_to_text_teacher_forcing_logits_model():
 
     tokenizer = AutoTokenizer.from_pretrained("gpt2")
     model =  AutoModelForCausalLM.from_pretrained("gpt2")
-    wrapped_model = TeacherForcingLogits(f,text_similarity_model=model, text_similarity_tokenizer=tokenizer)
-    masker = maskers.Text(tokenizer, mask_token = "<infill>")
+    wrapped_model = TeacherForcingLogits(f,similarity_model=model, similarity_tokenizer=tokenizer)
+    masker = maskers.Text(tokenizer, mask_token = "...")
     
     explainer = shap.Explainer(wrapped_model,masker)
 
