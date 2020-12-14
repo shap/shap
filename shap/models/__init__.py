463cdd18132b43117021d350d7bfa54139d5b7ea
ryserrao <serrao.ryan1995@gmail.com>
2020-12-13 19:27:52 -0500

    Changed file names of pytorch models with prefix _pt

--- shap/models/__init__.py
+++ shap/models/__init__.py
@@ -1,5 +1,5 @@
 from ._model import Model
-from ._teacher_forcing_logits import PTTeacherForcingLogits
+from ._pt_teacher_forcing_logits import PTTeacherForcingLogits
 from ._tf_teacher_forcing_logits import TFTeacherForcingLogits
-from ._text_generation import PTTextGeneration
+from ._pt_text_generation import PTTextGeneration
 from ._tf_text_generation import TFTextGeneration
