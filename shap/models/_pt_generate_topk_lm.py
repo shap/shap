e3164c6b9b4d13c163706f00d3e0d3fcc34d36dc
ryserrao <serrao.ryan1995@gmail.com>
2020-12-16 08:24:28 -0500

    Moved functions from child to parent class->GenerateTopKLM

--- shap/models/_pt_generate_topk_lm.py
+++ shap/models/_pt_generate_topk_lm.py
@@ -33,33 +33,6 @@ class PTGenerateTopKLM(GenerateTopKLM):
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') if self.device is None else self.device 
         self.model = model.to(self.device)
 
-    def __call__(self, masked_X, X):
-        """ Computes log odds scores for a given batch of masked inputs for the top-k tokens for Causal/Masked LM.
-
-        Parameters
-        ----------
-        masked_X: numpy.array
-            An array containing a list of masked inputs.
-
-        X: numpy.array
-            An array containing a list of original inputs
-
-        Returns
-        -------
-        numpy.array
-            A numpy array of log odds scores for topk tokens for every input pair (masked_X, X)
-        """
-        output_batch=[]
-        for masked_x, x in zip(masked_X, X):
-            # update target sentence ids and original input for a new explanation row
-            self.update_cache_X(x)
-            # pass the masked input from which to generate source sentence ids
-            sentence_ids = self.get_sentence_ids(masked_x)
-            logits = self.get_lm_logits(sentence_ids).numpy().astype('float64')
-            logodds = self.get_logodds(logits)
-            output_batch.append(logodds)
-        return np.array(output_batch)
-
     def get_sentence_ids(self, X):
         """ The function tokenizes sentence.
 
@@ -75,39 +48,6 @@ class PTGenerateTopKLM(GenerateTopKLM):
         """
         sentence_ids = torch.tensor([self.tokenizer.encode(X)], device=self.device).to(torch.int64)
         return sentence_ids
-
-    def update_cache_X(self, X):
-        """ The function updates original input(X) and top-k token ids for the Causal/Masked LM.
-
-        It mimics the caching mechanism to update the original input and topk token ids
-        that are to be explained and which updates for every new row of explanation.
-
-        Parameters
-        ----------
-        X: string
-            Input(Text) for an explanation row.
-        """
-        # check if the source sentence has been updated (occurs when explaining a new row)
-        if (self.X is None) or (self.X != X):
-            self.X = X
-            self.output_names = self.get_output_names_and_update_topk_token_ids(self.X)
-
-    def get_output_names_and_update_topk_token_ids(self, X):
-        """ Gets the token names for top-k token ids for Causal/Masked LM.
-        
-        Parameters
-        ----------
-        X: string
-            Input(Text) for an explanation row.
-
-        Returns
-        -------
-        list
-            A list of output tokens.
-        """
-        self.topk_token_ids = self.generate_topk_token_ids(X)
-        output_names = [self.tokenizer.decode([x]).strip() for x in self.topk_token_ids]
-        return output_names
 
     def generate_topk_token_ids(self, X):
         """ Generates top-k token ids for Causal/Masked LM.
@@ -161,25 +101,3 @@ class PTGenerateTopKLM(GenerateTopKLM):
             logits=outputs.logits.detach().cpu()[:,sentence_ids.shape[1]-1,:]
             del outputs    
         return logits
-
-    def get_logodds(self, logits):
-        """ Calculates log odds from logits.
-
-        This function passes the logits through softmax and then computes log odds for the top-k token ids.
-
-        Parameters
-        ----------
-        logits: numpy.array
-            An array of logits generated from the model.
-
-        Returns
-        -------
-        numpy.array
-            Computes log odds for corresponding target sentence ids.
-        """
-        # pass logits through softmax, get the token corresponding score and convert back to log odds (as one vs all)
-        probs = (np.exp(logits).T / np.exp(logits).sum(-1)).T
-        logit_dist = sp.special.logit(probs)
-        indices = np.tile(self.topk_token_ids, (logits.shape[0],1))
-        logodds = np.take(logit_dist, indices)
-        return logodds[0]
