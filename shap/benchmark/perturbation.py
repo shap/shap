8f3c7ab5b1ab1a86842253ae5d4210e6225e8274
Maggie Wu <wu.maggie96@gmail.com>
2020-12-17 19:01:13 -0500

    working notebooks

--- shap/benchmark/perturbation.py
+++ shap/benchmark/perturbation.py
@@ -1,6 +1,6 @@
-from shap.utils import safe_isinstance
+from shap.utils import safe_isinstance, MaskedModel
 from shap.maskers import Independent, Partition, Impute, Text, Image, FixedComposite
-from shap import Explanation
+from shap import Explanation, links
 import matplotlib.pyplot as pl
 import sklearn
 import numpy as np
@@ -9,9 +9,10 @@ import time
 
 
 class SequentialPerturbation():
-    def __init__(self, f, masker, sort_order, perturbation):
-        self.f = f
-        self.masker = masker
+    def __init__(self, model, masker, sort_order, perturbation):
+        # self.f = lambda masked, x, index: model.predict(masked)
+        self.model = model if callable(model) else model.predict
+        self.masker = masker 
         self.sort_order = sort_order
         self.perturbation = perturbation
         
@@ -24,30 +25,152 @@ class SequentialPerturbation():
             self.sort_order_map = lambda x: np.argsort(-abs(x))
         else:
             raise ValueError("sort_order must be either \"positive\", \"negative\", or \"absolute\"!")
-            
+        
+        # user must give valid masker 
+        underlying_masker = masker.masker if isinstance(masker, FixedComposite) else masker
+        if isinstance(underlying_masker, (Independent, Partition, Impute)):
+            self.data_type = "tabular"
+        elif isinstance(underlying_masker, Text):
+            self.data_type = "text"
+        elif isinstance(underlying_masker, Image):
+            self.data_type = "image"
+        else: 
+            raise ValueError("masker must be for \"tabular\", \"text\", or \"image\"!")
+
         self.score_values = []
         self.score_aucs = []
         self.labels = []
 
-    def score(self, explanation, X, percent=0.01, y=None, label=None, silent=False, debug_mode=False):
+    def model_score(self, explanation, X, percent=0.01, y=None, label=None, silent=False, debug_mode=False):
         # if explainer is already the attributions 
         if safe_isinstance(explanation, "numpy.ndarray"):
             attributions = explanation
         elif isinstance(explanation, Explanation):
             attributions = explanation.values
 
-        # user must give valid masker 
-        underlying_masker = self.masker.masker if isinstance(self.masker, FixedComposite) else self.masker
-        if isinstance(underlying_masker, (Independent, Partition, Impute)):
-            data_type = "tabular"
-        elif isinstance(underlying_masker, Text):
-            data_type = "text"
-        elif isinstance(underlying_masker, Image):
-            data_type = "image"
-        else: 
-            raise ValueError("masker is undefined!")
+        if label is None:
+            label = "Score %d" % len(self.score_values)
+        
+        # convert dataframes
+        if safe_isinstance(X, "pandas.core.series.Series") or safe_isinstance(X, "pandas.core.frame.DataFrame"):
+            X = X.values
 
-        self.masker = FixedComposite(underlying_masker)
+        # convert all single-sample vectors to matrices
+        if not hasattr(attributions[0], "__len__"):
+            attributions = np.array([attributions])
+        if not hasattr(X[0], "__len__") and self.data_type == "tabular":
+            X = np.array([X])
+
+        pbar = None
+        start_time = time.time()
+        svals = []
+        mask_vals = []
+
+        for i in range(len(X)):
+            if self.data_type == "image": 
+                x_shape, y_shape = attributions[i].shape[0], attributions[i].shape[1]
+                feature_size = np.prod([x_shape, y_shape])
+                sample_attributions = attributions[i].mean(2).reshape(feature_size, -1)
+                data = X[i].flatten()
+                mask_shape = X[i].shape 
+            else: 
+                feature_size = attributions[i].shape[0] 
+                sample_attributions = attributions[i] 
+                data = X[i]
+                mask_shape = feature_size
+            
+            self.masked_model = MaskedModel(self.model, self.masker, links.identity, data)
+
+            if len(attributions[i].shape) == 1 or self.data_type == "tabular": 
+                output_size = 1 
+            else: 
+                output_size = attributions[i].shape[-1]
+            
+            masks = []
+            for k in range(output_size): 
+                mask = np.ones(mask_shape, dtype=np.bool) * (self.perturbation == "remove")
+                masks.append(mask.copy().flatten())
+
+                if output_size != 1: 
+                    test_attributions = sample_attributions[:,k]
+                else: 
+                    test_attributions = sample_attributions
+                
+                ordered_inds = self.sort_order_map(test_attributions)
+                increment = max(1,int(feature_size*percent))
+                for j in range(0, feature_size, increment): 
+                    oind_list = [ordered_inds[l] for l in range(j, min(feature_size, j+increment))]
+
+                    for oind in oind_list: 
+                        if not ((self.sort_order == "positive" and test_attributions[oind] <= 0) or \
+                                (self.sort_order == "negative" and test_attributions[oind] >= 0)):
+                            if self.data_type == "image":
+                                xoind, yoind = oind // attributions[i].shape[1], oind % attributions[i].shape[1]
+                                mask[xoind][yoind] = self.perturbation == "keep"
+                            else: 
+                                mask[oind] = self.perturbation == "keep"
+
+                    masks.append(mask.copy().flatten())
+
+            mask_vals.append(masks)
+
+            mask_size = len(range(0, feature_size, increment))+1
+            values = self.masked_model(np.array(masks))
+            index = 0
+
+            for k in range(output_size):
+                if output_size == 1: 
+                    svals.append(values[index:index+mask_size])
+                else: 
+                    svals.append(values[index:index+mask_size,k])
+                index += mask_size 
+
+            if pbar is None and time.time() - start_time > 5:
+                pbar = tqdm(total=len(X), disable=silent, leave=False)
+                pbar.update(i+1)
+            if pbar is not None:
+                pbar.update(1)
+            
+        if pbar is not None:
+            pbar.close()
+        
+        self.score_values.append(np.array(svals))
+        
+        if self.sort_order == "negative": 
+            curve_sign = -1
+        else: 
+            curve_sign = 1
+        
+        self.labels.append(label)
+
+        xs = np.linspace(0, 1, 100)
+        curves = np.zeros((len(self.score_values[-1]), len(xs)))
+        for j in range(len(self.score_values[-1])):
+            xp = np.linspace(0, 1, len(self.score_values[-1][j]))
+            yp = self.score_values[-1][j]
+            curves[j,:] = np.interp(xs, xp, yp)
+        ys = curves.mean(0)
+        auc = sklearn.metrics.auc(np.linspace(0, 1, len(ys)), curve_sign*(ys-ys[0]))
+
+        if not debug_mode: 
+            return xs, ys, auc
+        else: 
+            aucs = []
+            for j in range(len(self.score_values[-1])):
+                curve = curves[j,:]
+                auc = sklearn.metrics.auc(np.linspace(0, 1, len(curve)), curve_sign*(curve-curve[0]))
+                aucs.append(auc)
+            return mask_vals, curves, aucs
+
+    def score(self, explanation, X, percent=0.01, y=None, label=None, silent=False, debug_mode=False):
+        '''
+        Will be deprecated once MaskedModel is in complete support 
+        '''
+        # if explainer is already the attributions 
+        if safe_isinstance(explanation, "numpy.ndarray"):
+            attributions = explanation
+        elif isinstance(explanation, Explanation):
+            attributions = explanation.values
 
         if label is None:
             label = "Score %d" % len(self.score_values)
@@ -59,7 +182,7 @@ class SequentialPerturbation():
         # convert all single-sample vectors to matrices
         if not hasattr(attributions[0], "__len__"):
             attributions = np.array([attributions])
-        if not hasattr(X[0], "__len__") and data_type == "tabular":
+        if not hasattr(X[0], "__len__") and self.data_type == "tabular":
             X = np.array([X])
 
         pbar = None
@@ -68,7 +191,7 @@ class SequentialPerturbation():
         mask_vals = []
 
         for i in range(len(X)): 
-            if data_type == "image": 
+            if self.data_type == "image": 
                 x_shape, y_shape = attributions[i].shape[0], attributions[i].shape[1]
                 feature_size = np.prod([x_shape, y_shape])
                 sample_attributions = attributions[i].mean(2).reshape(feature_size, -1)
@@ -76,13 +199,13 @@ class SequentialPerturbation():
                 feature_size = attributions[i].shape[0] 
                 sample_attributions = attributions[i] 
 
-            if len(attributions[i].shape) == 1 or data_type == "tabular": 
+            if len(attributions[i].shape) == 1 or self.data_type == "tabular": 
                 output_size = 1 
             else: 
                 output_size = attributions[i].shape[-1] 
             
             for k in range(output_size): 
-                if data_type == "image":
+                if self.data_type == "image":
                     mask_shape = X[i].shape 
                 else: 
                     mask_shape = feature_size 
@@ -91,7 +214,9 @@ class SequentialPerturbation():
                 masks = [mask.copy()]
 
                 values = np.zeros(feature_size+1)
-                masked, data = self.masker(mask, X[i])
+                # masked, data = self.masker(mask, X[i])
+                masked = self.masker(mask, X[i])
+                data = None 
                 curr_val = self.f(masked, data, k).mean(0)
                 
                 values[0] = curr_val 
@@ -109,14 +234,15 @@ class SequentialPerturbation():
                     for oind in oind_list: 
                         if not ((self.sort_order == "positive" and test_attributions[oind] <= 0) or \
                                 (self.sort_order == "negative" and test_attributions[oind] >= 0)):
-                            if data_type == "image":
+                            if self.data_type == "image":
                                 xoind, yoind = oind // attributions[i].shape[1], oind % attributions[i].shape[1]
                                 mask[xoind][yoind] = self.perturbation == "keep"
                             else: 
                                 mask[oind] = self.perturbation == "keep"
 
                     masks.append(mask.copy())
-                    masked, data = self.masker(mask, X[i])
+                    # masked, data = self.masker(mask, X[i])
+                    masked = self.masker(mask, X[i])
                     curr_val = self.f(masked, data, k).mean(0)
                     
                     for l in range(j, min(feature_size, j+increment)): 
@@ -133,7 +259,7 @@ class SequentialPerturbation():
             
         if pbar is not None:
             pbar.close()
-            
+
         self.score_values.append(np.array(svals))
         
         if self.sort_order == "negative": 
@@ -150,8 +276,6 @@ class SequentialPerturbation():
             yp = self.score_values[-1][j]
             curves[j,:] = np.interp(xs, xp, yp)
         ys = curves.mean(0)
-
-        auc = sklearn.metrics.auc(np.linspace(0, 1, len(ys)), curve_sign*(ys-ys[0]))
         
         if debug_mode: 
             aucs = []
@@ -166,7 +290,5 @@ class SequentialPerturbation():
         
     def plot(self, xs, ys, auc):
         pl.plot(xs, ys, label="AUC %0.4f" % auc)
-        if (self.sort_order == "negative") != (self.perturbation == "remove"):
-            pl.gca().invert_yaxis()
         pl.legend()
         pl.show()
--- shap/utils/_masked_model.py
+++ shap/utils/_masked_model.py
@@ -88,7 +88,7 @@ class MaskedModel():
 
             # wrap the masked inputs if they are not already in a tuple
             if not isinstance(masked_inputs, tuple):
-                masked_inputs = (masked_inputs,)
+                masked_inputs = (masked_inputs.copy(),)
                 
             # masked_inputs = self.masker(mask, *self.args)
             num_mask_samples[i] = len(masked_inputs[0])
@@ -114,7 +114,7 @@ class MaskedModel():
 
             # subset the masked input to only the rows that vary
             if num_varying_rows[i] != num_mask_samples[i]:
-                if len(masked_inputs) == 1:
+                if len(self.args) == 1:
                     # _ = masked_inputs[varying_rows[-1]]
                     # _ = masked_inputs[varying_rows[-1]]
                     # _ = masked_inputs[varying_rows[-1]]
