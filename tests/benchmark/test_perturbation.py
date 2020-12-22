90d293ec72067296b738a50d534a6e1ffab36cdc
Vivek Chettiar <vivekrchettiar@gmail.com>
2020-12-22 18:51:55 -0500

    Moved imports and initialization inside test function, in benchmark tests

--- tests/benchmark/test_perturbation.py
+++ tests/benchmark/test_perturbation.py
@@ -1,15 +1,15 @@
-import numpy as np
-import shap 
-import shap.benchmark as benchmark 
-from transformers import AutoTokenizer
-from shap.maskers import Independent, Partition, Impute, Text, Image, FixedComposite
-
-model = lambda x, y: x 
-sort_order = 'positive'
-perturbation = 'keep'
-X = np.random.random((10,13))
-
 def test_init():
+    import numpy as np
+    import shap 
+    import shap.benchmark as benchmark 
+    from transformers import AutoTokenizer
+    from shap.maskers import Independent, Partition, Impute, Text, Image, FixedComposite
+
+    model = lambda x, y: x 
+    sort_order = 'positive'
+    perturbation = 'keep'
+    X = np.random.random((10,13))
+
     tabular_masker = Independent(X)
     sequential_perturbation = benchmark.perturbation.SequentialPerturbation(model, tabular_masker, sort_order, perturbation)
     assert sequential_perturbation.data_type == "tabular" 
