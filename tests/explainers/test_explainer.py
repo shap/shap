60cae27ef2dd3629f88b2799f9104f74b68315a9
ryserrao <serrao.ryan1995@gmail.com>
2021-01-21 03:07:54 -0500

    Updated tests for TopKLM masker auto-wrapper

--- tests/explainers/test_explainer.py
+++ tests/explainers/test_explainer.py
@@ -32,8 +32,8 @@ def test_wrapping_for_topk_lm_model():
     tokenizer = transformers.AutoTokenizer.from_pretrained("gpt2")
     model = transformers.AutoModelForCausalLM.from_pretrained("gpt2")
     wrapped_model = shap.models.TopKLM(model, tokenizer)
+    masker = shap.maskers.Text(tokenizer, mask_token="...")
 
-    explainer = shap.Explainer(wrapped_model, tokenizer)
+    explainer = shap.Explainer(wrapped_model, masker)
 
     assert shap.utils.safe_isinstance(explainer.masker, "shap.maskers.FixedComposite")
-
