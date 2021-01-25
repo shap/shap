e24d8ce2bb0d517551a7e8dd98d8c445b59a426d
jsu27 <jocelin.su@gmail.com>
2021-01-24 17:47:08 -0800

    added example notebook and file for 1 arg SHAP for text entailment

--- notebooks/text_examples/text_entailment/bart_nli.py
+++ notebooks/text_examples/text_entailment/bart_nli.py
@@ -11,32 +11,67 @@
 import numpy as np
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 import shap
+from datasets import load_dataset
 
 # ### Load model and tokenizer
 
+tokenizer = AutoTokenizer.from_pretrained("facebook/bart-large-mnli", use_fast=False)
+model = AutoModelForSequenceClassification.from_pretrained("facebook/bart-large-mnli")
+
+# dataset
+dataset = load_dataset("snli")
+print(dataset)
+
+example_ind = 6
+premise, hypothesis, label = dataset['train']['premise'][example_ind], dataset['train']['hypothesis'][example_ind], dataset['train']['label'][example_ind]
+print(premise)
+print(hypothesis)
+print(label)
+# p2, h2, l2 = (
+#     dataset["train"]["premise"][example_ind + 1],
+#     dataset["train"]["hypothesis"][example_ind + 1],
+#     dataset["train"]["label"][example_ind + 1]
+# )
+# print(p2)
+# print(h2)
+# print(l2)
+
+
+input_ids = tokenizer.encode(premise, hypothesis, return_tensors='pt')
+print(input_ids)
+logits = model(input_ids)[0]
+
+probs = logits.softmax(dim=1)
+p1, p2, p3 = probs[:,0].item() * 100, probs[:,1].item()*100, probs[:,2].item()*100
+print('Contradiction Probability: {p1:0.2f}%, Neutral Probability: {p2:0.2f}%, Entailment Probability: {p3:0.2f}%'.format(p1 = p1, p2 = p2, p3=p3))
+ind_to_label = {0: 'entailment', 1: 'neural', 2: 'contradiction'}
+true_label = ind_to_label[label]
+print('The true label is: {true_label}'.format(true_label=true_label))
+
+###
+
+
 # In[2]:
 
-model = AutoModelForSequenceClassification.from_pretrained('joeddav/xlm-roberta-large-xnli')
-tokenizer = AutoTokenizer.from_pretrained('joeddav/xlm-roberta-large-xnli', use_fast=False) # TODO changed use_fast to False
 device = 'cpu'
 
 candidate_labels = ['travel', 'cooking', 'dancing', 'exploration']
 label = candidate_labels[0]
-sequence = 'I want to go to Japan.'
-premise = sequence
-hypothesis = f'This example is {label}.'
+# premise = 'I want to go to Japan.'
+# hypothesis = f'This example is {label}.'
 # TODO data = load_data('nli') # note: mnli is not loadable with this environment
-
+premise = 'A boy is jumping on skateboard in the middle of a red bridge.'
+hypothesis = 'A boy skates down the sidewalk.'
 # run through model pre-trained on MNLI
-x = tokenizer.encode(premise, hypothesis, return_tensors='pt',
-                     truncation_strategy='only_first')
+# x = tokenizer.encode(premise, hypothesis, return_tensors='pt',
+#                      truncation_strategy='only_first')
 
-print('INPUT')
-print(x)
+# print('INPUT')
+# print(x)
 
-logits = model(x.to(device))[0]
-print('LOGITS')
-print(logits)
+# logits = model(x.to(device))[0]
+# print('LOGITS')
+# print(logits)
 
 import scipy as sp
 import torch
@@ -49,7 +84,6 @@ import torch
 #     return val
 
 def f(x): # TODO takes in the already - masked string which is the concatenation of 2 strings
-    # tv = torch.tensor([tokenizer.encode(*_x) for _x in zip(*x)])
     tv = torch.tensor([tokenizer.encode(_x) for _x in x])
     outputs = model(tv)[0].detach().cpu().numpy()
     scores = (np.exp(outputs).T / np.exp(outputs).sum(-1)).T
@@ -67,9 +101,20 @@ encoded = tokenizer(premise, hypothesis)['input_ids'][1:-1]
 decoded = tokenizer.decode(encoded)
 print(decoded)
 
+
+# testing tokenizer
+
+s1 = 'I ate some pie.'
+s2 = 'You are a dog.'
+encode1 = tokenizer(s1)
+encode2 = tokenizer(s2) 
+print(encode1)
+print(encode2)
+# 
+# 
 # shap_values = explainer([premise], [hypothesis]) # wrap in list, otherwise zip would iterate across letters
 shap_values = explainer([decoded]) # wrap in list, otherwise zip would iterate across letters
 
-# print('SHAP VALUES')
+print('SHAP VALUES')
 # shap_values = explainer([premise], [hypothesis]) # wrap in list, otherwise zip would iterate across letters
 print(shap_values)
