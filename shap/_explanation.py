Fix slicing issues and Text masking with Permutation explainer

shap/_explanation.py
@@ -99,10 +99,22 @@ class Explanation(object, metaclass=MetaExplanation):
 
         if len(_compute_shape(output_names)) == 1: # TODO: should always be an alias once slicer supports per-row aliases
             values_shape = _compute_shape(values)
-            if len(values_shape) >= 1 and len(output_names) == values_shape[0]:
-                output_names = Alias(list(output_names), 0)
-            elif len(values_shape) >= 2 and len(output_names) == values_shape[1]:
-                output_names = Alias(list(output_names), 1)
+            output_names = Alias(list(output_names), output_dims[0])
+            # if len(values_shape) >= 1 and len(output_names) == values_shape[0]:
+            #     output_names = Alias(list(output_names), 0)
+            # elif len(values_shape) >= 2 and len(output_names) == values_shape[1]:
+            #     output_names = Alias(list(output_names), 1)
+
+        if output_names is not None and not isinstance(output_names, Alias):
+            l = len(_compute_shape(output_names))
+            if l == 0:
+                pass
+            elif l == 1:
+                output_names = Obj(output_names, output_dims)
+            elif l == 2:
+                output_names = Obj(output_names, [0] + list(output_dims))
+            else:
+                raise ValueError("shap.Explanation does not yet support output_names of order greater than 3!")
 
         self._s = Slicer(
             values=values,
@@ -111,7 +123,7 @@ class Explanation(object, metaclass=MetaExplanation):
             display_data=display_data,
             instance_names=None if instance_names is None else Alias(instance_names, 0),
             feature_names=feature_names,
-            output_names= output_names, # None if output_names is None else Alias(output_names, output_dims),
+            output_names=output_names,
             output_indexes=None if output_indexes is None else (output_dims, output_indexes),
             lower_bounds=lower_bounds,
             upper_bounds=upper_bounds,
@@ -232,13 +244,13 @@ class Explanation(object, metaclass=MetaExplanation):
         return out
 
     def __getitem__(self, item):
-        """ This adds support for magic string indexes like "rank(0)".
+        """ This adds support for OpChain indexing.
         """
         if not isinstance(item, tuple):
             item = (item,)
 
         # convert any OpChains or magic strings
-        for i, t in enumerate(item):
+        for pos, t in enumerate(item):
             orig_t = t
             if issubclass(type(t), OpChain):
                 t = t.apply(self)
@@ -249,47 +261,56 @@ class Explanation(object, metaclass=MetaExplanation):
             elif issubclass(type(t), Explanation):
                 t = t.values
             elif isinstance(t, str):
-                if is_1d(self.feature_names):
-                    ind = np.where(np.array(self.feature_names) == t)[0][0]
-                    t = int(ind)
-                else:
+
+                # work around for 2D output_names since they are not yet slicer supported
+                output_names_dims = []
+                if "output_names" in self._s._objects:
+                    output_names_dims = self._s._objects["output_names"].dim
+                if pos != 0 and pos in output_names_dims and len(output_names_dims) == 2:
                     new_values = []
                     new_base_values = []
                     new_data = []
-                    if self.output_names is not None and (self.output_names.ndim >= 2 or self.output_names.shape[0] >= 2):
-                        new_self = copy.deepcopy(self)
-                        for i in range(len(self.values)):
-                            for j in range(len(self.output_names[i])): 
-                                s = self.output_names[i][j]
-                                if s == t: 
-                                    new_values.append(np.array(self.values[i][:,j]))
-                                    new_data.append(np.array(self.data[i]))
-                                    new_base_values.append(self.base_values[i][j])
-                        new_self = copy.deepcopy(self)
-                        new_self.values = np.array(new_values)
-                        new_self.base_values = np.array(new_base_values)
-                        new_self.data = np.array(new_data)
-                        new_self.output_names = t
-                        new_self.feature_names = np.array(new_data)
-                        new_self.clustering = None
-                    else: 
-                        for i in range(len(self.values)):
-                            for s,v,d in zip(self.feature_names[i], self.values[i], self.data[i]):
-                                if s == t:
-                                    new_values.append(v)
-                                    new_data.append(d)
-                        new_self = copy.deepcopy(self)
-                        new_self.values = new_values
-                        new_self.data = new_data
-                        new_self.feature_names = t
-                        new_self.clustering = None
+                    new_self = copy.deepcopy(self)
+                    for i in range(len(self.values)):
+                        for j in range(len(self.output_names[i])):
+                            s = self.output_names[i][j]
+                            if s == t:
+                                new_values.append(np.array(self.values[i][:,j]))
+                                new_data.append(np.array(self.data[i]))
+                                new_base_values.append(self.base_values[i][j])
+                    new_self = copy.deepcopy(self)
+                    new_self.values = np.array(new_values)
+                    new_self.base_values = np.array(new_base_values)
+                    new_self.data = np.array(new_data)
+                    new_self.output_names = t
+                    new_self.feature_names = np.array(new_data)
+                    new_self.clustering = None
+
+                # work around for 2D feature_names since they are not yet slicer supported
+                feature_names_dims = []
+                if "feature_names" in self._s._objects:
+                    feature_names_dims = self._s._objects["feature_names"].dim
+                if pos != 0 and pos in feature_names_dims and len(feature_names_dims) == 2:
+                    new_values = []
+                    new_data = []
+                    for i in range(len(self.values)):
+                        for s,v,d in zip(self.feature_names[i], self.values[i], self.data[i]):
+                            if s == t:
+                                new_values.append(v)
+                                new_data.append(d)
+                    new_self = copy.deepcopy(self)
+                    new_self.values = new_values
+                    new_self.data = new_data
+                    new_self.feature_names = t
+                    new_self.clustering = None
                     return new_self
+
             if issubclass(type(t), (np.int8, np.int16, np.int32, np.int64)):
                 t = int(t)
 
             if t is not orig_t:
                 tmp = list(item)
-                tmp[i] = t
+                tmp[pos] = t
                 item = tuple(tmp)
 
         # call slicer for the real work
@@ -626,7 +647,7 @@ class Percentile(Op):
 
 
 def _compute_shape(x):
-    if not hasattr(x, "__len__"):
+    if not hasattr(x, "__len__") or isinstance(x, str):
         return tuple()
     elif not sp.sparse.issparse(x) and len(x) > 0 and isinstance(x[0], str):
         return (None,)

shap/explainers/_explainer.py
@@ -288,13 +288,15 @@ class Explainer(Serializable):
         clustering = pack_values(clustering)
 
         # getting output labels
+        ragged_outputs = False
         if output_indices is not None:
             ragged_outputs = not all(len(x) == len(output_indices[0]) for x in output_indices)
         if self.output_names is None:
             if None not in output_names:
-                sliced_labels = [np.array(output_names[i])[index_list] for i,index_list in enumerate(output_indices)]
                 if not ragged_outputs:
-                    sliced_labels = np.array(sliced_labels)
+                    sliced_labels = np.array(output_names)
+                else:
+                    sliced_labels = [np.array(output_names[i])[index_list] for i,index_list in enumerate(output_indices)]
             else:
                 sliced_labels = None
         else:
@@ -302,6 +304,10 @@ class Explainer(Serializable):
             sliced_labels = [labels[index_list] for index_list in output_indices]
             if not ragged_outputs:
                 sliced_labels = np.array(sliced_labels)
+
+        if isinstance(sliced_labels, np.ndarray) and len(sliced_labels.shape) == 2:
+            if np.all(sliced_labels[0,:] == sliced_labels):
+                sliced_labels = sliced_labels[0]
 
         # build the explanation objects
         out = []

shap/explainers/_partition.py
@@ -111,12 +111,12 @@ class Partition(Explainer):
         """
 
         if fixed_context == "auto":
+            # if isinstance(self.masker, maskers.Text):
+            #     fixed_context = 1 # we err on the side of speed for text models
+            # else:
             fixed_context = None
-        elif fixed_context in [0, 1, None]:
-            fixed_context = fixed_context
-        else:
+        elif fixed_context not in [0, 1, None]:
             raise Exception("Unknown fixed_context value passed (must be 0, 1 or None): %s" %fixed_context)
-
 
         # build a masked version of the model for the current input sample
         fm = MaskedModel(self.model, self.masker, self.link, *row_args)
@@ -143,7 +143,6 @@ class Partition(Explainer):
         else:
             out_shape = (2*self._clustering.shape[0]+1,)
 
-
         if max_evals == "auto":
             max_evals = 100
 
@@ -160,7 +159,7 @@ class Partition(Explainer):
         # else:
         # drop the interaction terms down onto self.values
         self.values[:] = self.dvalues
-        
+
         def lower_credit(i, value=0):
             if i < M:
                 self.values[i] += value
@@ -175,7 +174,7 @@ class Partition(Explainer):
             lower_credit(li, self.values[i] * lsize / group_size)
             lower_credit(ri, self.values[i] * rsize / group_size)
         lower_credit(len(self.dvalues) - 1)
-            
+
         return {
             "values": self.values[:M].copy(),
             "expected_values": self._curr_base_value if outputs is None else self._curr_base_value[outputs],
@@ -190,7 +189,7 @@ class Partition(Explainer):
     def owen(self, fm, f00, f11, max_evals, output_indexes, fixed_context, batch_size, silent):
         """ Compute a nested set of recursive Owen values based on an ordering recursion.
         """
-        
+
         #f = self._reshaped_model
         #r = self.masker
         #masks = np.zeros(2*len(inds)+1, dtype=np.int)
@@ -263,19 +262,19 @@ class Partition(Explainer):
                 m10[:] += self._mask_matrix[lind, :]
                 m01 = m00.copy()
                 m01[:] += self._mask_matrix[rind, :]
-                
+
                 batch_args.append((m00, m10, m01, f00, f11, ind, lind, rind, weight))
                 batch_masks.append(m10)
                 batch_masks.append(m01)
 
             batch_masks = np.array(batch_masks)
-                
+
             # run the batch
             if len(batch_args) > 0:
                 fout = fm(batch_masks)
                 if output_indexes is not None:
                     fout = fout[:,output_indexes]
-                    
+
                 eval_count += len(batch_masks)
 
                 if pbar is None and time.time() - start_time > 5:
@@ -286,7 +285,7 @@ class Partition(Explainer):
 
             # use the results of the batch to add new nodes
             for i in range(len(batch_args)):
-                
+
                 m00, m10, m01, f00, f11, ind, lind, rind, weight = batch_args[i]
 
                 # get the evaluated model output on the two new masked inputs

shap/explainers/_permutation.py
@@ -41,9 +41,9 @@ class Permutation(Explainer):
             game structure you can pass a shap.maksers.Tabular(data, clustering=\"correlation\") object.
         """
         super(Permutation, self).__init__(model, masker, link=link, feature_names=feature_names)
-        
-        self.model = Model(model)
 
+        if not isinstance(model, Model):
+            self.model = Model(model)
 
     def explain_row(self, *row_args, max_evals, main_effects, error_bounds, batch_size, outputs, silent):
         """ Explains a single row and returns the tuple (row_values, row_expected_values, row_mask_shapes).
@@ -94,33 +94,37 @@ class Permutation(Explainer):
                 for ind in inds:
                     masks[i] = ind
                     i += 1
-                
+
                 # evaluate the masked model
                 outputs = fm(masks, batch_size=batch_size)
 
                 if row_values is None:
                     row_values = np.zeros((len(fm),) + outputs.shape[1:])
-                
+
                 # update our SHAP value estimates
                 for i,ind in enumerate(inds):
                     row_values[ind] += outputs[i+1] - outputs[i]
                 for i,ind in enumerate(inds):
                     row_values[ind] += outputs[i+1] - outputs[i]
-            
+
+            if npermutations == 0:
+                raise Exception("max_evals is too low for the Permutation explainer, it must be at least 2 * num_features + 1!")
+
             expected_value = outputs[0]
 
             # compute the main effects if we need to
             if main_effects:
                 main_effect_values = fm.main_effects(inds)
-        
+
         return {
             "values": row_values / (2 * npermutations),
             "expected_values": expected_value,
             "mask_shapes": fm.mask_shapes,
             "main_effects": main_effect_values,
-            "clustering": row_clustering
+            "clustering": row_clustering,
+            "output_names": self.model.output_names if hasattr(self.model, "output_names") else None
         }
-    
+
 
     def shap_values(self, X, npermutations=10, main_effects=False, error_bounds=False, batch_evals=True, silent=False):
         """ Legacy interface to estimate the SHAP values for a set of samples.

tests/explainers/test_deep.py
@@ -2,6 +2,7 @@
 """
 
 from distutils.version import LooseVersion
+from urllib.error import HTTPError
 import numpy as np
 import pandas as pd
 import pytest
@@ -306,20 +307,23 @@ def test_pytorch_mnist_cnn(tmpdir):
 
     batch_size = 128
 
-    train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST(tmpdir, train=True, download=True,
-                       transform=transforms.Compose([
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.1307,), (0.3081,))
-                       ])),
-        batch_size=batch_size, shuffle=True)
-    test_loader = torch.utils.data.DataLoader(
-        datasets.MNIST(tmpdir, train=False, download=True,
-                       transform=transforms.Compose([
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.1307,), (0.3081,))
-                       ])),
-        batch_size=batch_size, shuffle=True)
+    try:
+        train_loader = torch.utils.data.DataLoader(
+            datasets.MNIST(tmpdir, train=True, download=True,
+                        transform=transforms.Compose([
+                            transforms.ToTensor(),
+                            transforms.Normalize((0.1307,), (0.3081,))
+                        ])),
+            batch_size=batch_size, shuffle=True)
+        test_loader = torch.utils.data.DataLoader(
+            datasets.MNIST(tmpdir, train=False, download=True,
+                        transform=transforms.Compose([
+                            transforms.ToTensor(),
+                            transforms.Normalize((0.1307,), (0.3081,))
+                        ])),
+            batch_size=batch_size, shuffle=True)
+    except HTTPError:
+        pytest.skip()
 
     #print('Running test on interim layer')
     run_test(train_loader, test_loader, interim=True)

tests/explainers/test_gradient.py
@@ -1,3 +1,4 @@
+from urllib.error import HTTPError
 import numpy as np
 import pytest
 import shap
@@ -98,20 +99,23 @@ def test_pytorch_mnist_cnn(tmpdir):
 
     batch_size = 128
 
-    train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST(tmpdir, train=True, download=True,
-                       transform=transforms.Compose([
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.1307,), (0.3081,))
-                       ])),
-        batch_size=batch_size, shuffle=True)
-    test_loader = torch.utils.data.DataLoader(
-        datasets.MNIST(tmpdir, train=False, download=True,
-                       transform=transforms.Compose([
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.1307,), (0.3081,))
-                       ])),
-        batch_size=batch_size, shuffle=True)
+    try:
+        train_loader = torch.utils.data.DataLoader(
+            datasets.MNIST(tmpdir, train=True, download=True,
+                        transform=transforms.Compose([
+                            transforms.ToTensor(),
+                            transforms.Normalize((0.1307,), (0.3081,))
+                        ])),
+            batch_size=batch_size, shuffle=True)
+        test_loader = torch.utils.data.DataLoader(
+            datasets.MNIST(tmpdir, train=False, download=True,
+                        transform=transforms.Compose([
+                            transforms.ToTensor(),
+                            transforms.Normalize((0.1307,), (0.3081,))
+                        ])),
+            batch_size=batch_size, shuffle=True)
+    except HTTPError:
+        pytest.skip()
 
     def run_test(train_loader, test_loader, interim):
 