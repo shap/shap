1eeddef2d0db3d6516caf1a80afac2c150e6ce89
ekrell <ekrell@islander.tamucc.edu>
2021-04-15 20:51:01 -0500

    Fixed indent error

--- shap/plots/_image.py
+++ shap/plots/_image.py
@@ -21,131 +21,131 @@ from ..utils._legacy import kmeans
 
 
 
- def image(shap_values, pixel_values=None, labels=None, width=20, aspect=0.2, hspace=0.2, labelpad=None, show=True, plotchannels=[0]):
-     """ Plots SHAP values for image inputs.
+def image(shap_values, pixel_values=None, labels=None, width=20, aspect=0.2, hspace=0.2, labelpad=None, show=True, plotchannels=[0]):
+    """ Plots SHAP values for image inputs.
 
-     Parameters
-     ----------
-     shap_values : [numpy.array]
-         List of arrays of SHAP values. Each array has the shap (# samples x width x height x channels), and the
-         length of the list is equal to the number of model outputs that are being explained.
+    Parameters
+    ----------
+    shap_values : [numpy.array]
+        List of arrays of SHAP values. Each array has the shap (# samples x width x height x channels), and the
+        length of the list is equal to the number of model outputs that are being explained.
 
-     pixel_values : numpy.array
-         Matrix of pixel values (# samples x width x height x channels) for each image. It should be the same
-         shape as each array in the shap_values list of arrays.
+    pixel_values : numpy.array
+        Matrix of pixel values (# samples x width x height x channels) for each image. It should be the same
+        shape as each array in the shap_values list of arrays.
 
-     labels : list
-         List of names for each of the model outputs that are being explained. This list should be the same length
-         as the shap_values list.
+    labels : list
+        List of names for each of the model outputs that are being explained. This list should be the same length
+        as the shap_values list.
 
-     width : float
-         The width of the produced matplotlib plot.
+    width : float
+        The width of the produced matplotlib plot.
 
-     labelpad : float
-         How much padding to use around the model output labels.
+    labelpad : float
+        How much padding to use around the model output labels.
 
-     show : bool
-         Whether matplotlib.pyplot.show() is called before returning. Setting this to False allows the plot
-         to be customized further after it has been created.
-     """
+    show : bool
+        Whether matplotlib.pyplot.show() is called before returning. Setting this to False allows the plot
+        to be customized further after it has been created.
+    """
 
-     # support passing an explanation object
-     if str(type(shap_values)).endswith("Explanation'>"):
-         shap_exp = shap_values
-         feature_names = [shap_exp.feature_names]
-         ind = 0
-         if len(shap_exp.base_values.shape) == 2:
-             shap_values = [shap_exp.values[..., i] for i in range(shap_exp.values.shape[-1])]
-         else:
-             raise Exception("Number of outputs needs to have support added!! (probably a simple fix)")
-         if pixel_values is None:
-             pixel_values = shap_exp.data
-         if labels is None:
-             labels = shap_exp.output_names
+    # support passing an explanation object
+    if str(type(shap_values)).endswith("Explanation'>"):
+        shap_exp = shap_values
+        feature_names = [shap_exp.feature_names]
+        ind = 0
+        if len(shap_exp.base_values.shape) == 2:
+            shap_values = [shap_exp.values[..., i] for i in range(shap_exp.values.shape[-1])]
+        else:
+            raise Exception("Number of outputs needs to have support added!! (probably a simple fix)")
+        if pixel_values is None:
+            pixel_values = shap_exp.data
+        if labels is None:
+            labels = shap_exp.output_names
 
-     multi_output = True
-     if type(shap_values) != list:
-         multi_output = False
-         shap_values = [shap_values]
+    multi_output = True
+    if type(shap_values) != list:
+        multi_output = False
+        shap_values = [shap_values]
 
-     # make sure the number of channels to plot is <= number of channels present
-     nchannels = min(shap_values[0].shape[-1], len(plotchannels))
-     plotchannels = plotchannels[:nchannels]
+    # make sure the number of channels to plot is <= number of channels present
+    nchannels = min(shap_values[0].shape[-1], len(plotchannels))
+    plotchannels = plotchannels[:nchannels]
 
-     # make sure labels
-     if labels is not None:
-         labels = np.array(labels)
-         assert labels.shape[0] == shap_values[0].shape[0], "Labels must have same row count as shap_values arrays!"
-         if multi_output:
-             assert labels.shape[1] == len(shap_values), "Labels must have a column for each output in shap_values!"
-         else:
-             assert len(labels.shape) == 1, "Labels must be a vector for single output shap_values."
+    # make sure labels
+    if labels is not None:
+        labels = np.array(labels)
+        assert labels.shape[0] == shap_values[0].shape[0], "Labels must have same row count as shap_values arrays!"
+        if multi_output:
+            assert labels.shape[1] == len(shap_values), "Labels must have a column for each output in shap_values!"
+        else:
+            assert len(labels.shape) == 1, "Labels must be a vector for single output shap_values."
 
-     label_kwargs = {} if labelpad is None else {'pad': labelpad}
+    label_kwargs = {} if labelpad is None else {'pad': labelpad}
 
-     # plot our explanations
-     x = pixel_values
-     fig_size = np.array([3 * (len(shap_values) + 1), 2.5 * (x.shape[0] + nchannels)])
-     if fig_size[0] > width:
-         fig_size *= width / fig_size[0]
-     fig, axes = pl.subplots(nrows=x.shape[0] * nchannels, ncols=len(shap_values) + 1, figsize=fig_size)
-     if len(axes.shape) == 1:
-         axes = axes.reshape(1,axes.size)
-     for row in range(x.shape[0]):
-         x_curr = x[row].copy()
+    # plot our explanations
+    x = pixel_values
+    fig_size = np.array([3 * (len(shap_values) + 1), 2.5 * (x.shape[0] + nchannels)])
+    if fig_size[0] > width:
+        fig_size *= width / fig_size[0]
+    fig, axes = pl.subplots(nrows=x.shape[0] * nchannels, ncols=len(shap_values) + 1, figsize=fig_size)
+    if len(axes.shape) == 1:
+        axes = axes.reshape(1,axes.size)
+    for row in range(x.shape[0]):
+        x_curr = x[row].copy()
 
-         # make sure we have a 2D array for grayscale
-         if len(x_curr.shape) == 3 and x_curr.shape[2] == 1:
-             x_curr = x_curr.reshape(x_curr.shape[:2])
-         if x_curr.max() > 1:
-             x_curr /= 255.
+        # make sure we have a 2D array for grayscale
+        if len(x_curr.shape) == 3 and x_curr.shape[2] == 1:
+            x_curr = x_curr.reshape(x_curr.shape[:2])
+        if x_curr.max() > 1:
+            x_curr /= 255.
 
-         # get a grayscale version of the image
-         if len(x_curr.shape) == 3 and x_curr.shape[2] == 3:
-             x_curr_gray = (0.2989 * x_curr[:,:,0] + 0.5870 * x_curr[:,:,1] + 0.1140 * x_curr[:,:,2]) # rgb to gray
-             x_curr_disp = x_curr
-         elif len(x_curr.shape) == 3:
-             x_curr_gray = x_curr.mean(2)
+        # get a grayscale version of the image
+        if len(x_curr.shape) == 3 and x_curr.shape[2] == 3:
+            x_curr_gray = (0.2989 * x_curr[:,:,0] + 0.5870 * x_curr[:,:,1] + 0.1140 * x_curr[:,:,2]) # rgb to gray
+            x_curr_disp = x_curr
+        elif len(x_curr.shape) == 3:
+            x_curr_gray = x_curr.mean(2)
 
-             # for non-RGB multi-channel data we show an RGB image where each of the three channels is a scaled k-mean center
-             flat_vals = x_curr.reshape([x_curr.shape[0]*x_curr.shape[1], x_curr.shape[2]]).T
-             flat_vals = (flat_vals.T - flat_vals.mean(1)).T
-             means = kmeans(flat_vals, 3, round_values=False).data.T.reshape([x_curr.shape[0], x_curr.shape[1], 3])
-             x_curr_disp = (means - np.percentile(means, 0.5, (0,1))) / (np.percentile(means, 99.5, (0,1)) - np.percentile(means, 1, (0,1)))
-             x_curr_disp[x_curr_disp > 1] = 1
-             x_curr_disp[x_curr_disp < 0] = 0
-         else:
-             x_curr_gray = x_curr
-             x_curr_disp = x_curr
+            # for non-RGB multi-channel data we show an RGB image where each of the three channels is a scaled k-mean center
+            flat_vals = x_curr.reshape([x_curr.shape[0]*x_curr.shape[1], x_curr.shape[2]]).T
+            flat_vals = (flat_vals.T - flat_vals.mean(1)).T
+            means = kmeans(flat_vals, 3, round_values=False).data.T.reshape([x_curr.shape[0], x_curr.shape[1], 3])
+            x_curr_disp = (means - np.percentile(means, 0.5, (0,1))) / (np.percentile(means, 99.5, (0,1)) - np.percentile(means, 1, (0,1)))
+            x_curr_disp[x_curr_disp > 1] = 1
+            x_curr_disp[x_curr_disp < 0] = 0
+        else:
+            x_curr_gray = x_curr
+            x_curr_disp = x_curr
 
-         axes[row*nchannels,0].imshow(x_curr_disp, cmap=pl.get_cmap('gray'))
-         axes[row*nchannels,0].axis('off')
-         for c in range(1, nchannels):
-             axes[row*nchannels+c, 0].set_visible(False)
+        axes[row*nchannels,0].imshow(x_curr_disp, cmap=pl.get_cmap('gray'))
+        axes[row*nchannels,0].axis('off')
+        for c in range(1, nchannels):
+            axes[row*nchannels+c, 0].set_visible(False)
 
-         if len(shap_values[0][row].shape) == 2:
-             abs_vals = np.stack([np.abs(shap_values[i]) for i in range(len(shap_values))], 0).flatten()
-         else:
-             abs_vals = np.stack([np.abs(shap_values[i].sum(-1)) for i in range(len(shap_values))], 0).flatten()
-         max_val = np.nanpercentile(abs_vals, 99.9)
-         for i in range(len(shap_values)):
-             if labels is not None:
-                 axes[row*nchannels,i+1].set_title(labels[row,i], **label_kwargs)
+        if len(shap_values[0][row].shape) == 2:
+            abs_vals = np.stack([np.abs(shap_values[i]) for i in range(len(shap_values))], 0).flatten()
+        else:
+            abs_vals = np.stack([np.abs(shap_values[i].sum(-1)) for i in range(len(shap_values))], 0).flatten()
+        max_val = np.nanpercentile(abs_vals, 99.9)
+        for i in range(len(shap_values)):
+            if labels is not None:
+                axes[row*nchannels,i+1].set_title(labels[row,i], **label_kwargs)
 
-             for c in range(nchannels):
-                 sv = shap_values[i][row] if len(shap_values[i][row].shape) == 2 else shap_values[i][row][..., plotchannels[c]]
-                 axes[row*nchannels+c,i+1].imshow(x_curr_gray, cmap=pl.get_cmap('gray'), alpha=0.15, extent=(-1, sv.shape[1], sv.shape[0], -1))
-                 im = axes[row*nchannels+c,i+1].imshow(sv, cmap=colors.red_transparent_blue, vmin=-max_val, vmax=max_val)
-                 axes[row*nchannels+c,i+1].axis('off')
+            for c in range(nchannels):
+                sv = shap_values[i][row] if len(shap_values[i][row].shape) == 2 else shap_values[i][row][..., plotchannels[c]]
+                axes[row*nchannels+c,i+1].imshow(x_curr_gray, cmap=pl.get_cmap('gray'), alpha=0.15, extent=(-1, sv.shape[1], sv.shape[0], -1))
+                im = axes[row*nchannels+c,i+1].imshow(sv, cmap=colors.red_transparent_blue, vmin=-max_val, vmax=max_val)
+                axes[row*nchannels+c,i+1].axis('off')
 
-     if hspace == 'auto':
-         fig.tight_layout()
-     else:
-         fig.subplots_adjust(hspace=hspace)
-     cb = fig.colorbar(im, ax=np.ravel(axes).tolist(), label="SHAP value", orientation="horizontal", aspect=fig_size[0]/aspect)
-     cb.outline.set_visible(False)
-     if show:
-         pl.show()
+    if hspace == 'auto':
+        fig.tight_layout()
+    else:
+        fig.subplots_adjust(hspace=hspace)
+    cb = fig.colorbar(im, ax=np.ravel(axes).tolist(), label="SHAP value", orientation="horizontal", aspect=fig_size[0]/aspect)
+    cb.outline.set_visible(False)
+    if show:
+        pl.show()
 
 
 def image_to_text(shap_values):
