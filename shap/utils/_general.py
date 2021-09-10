4397026b7e85c454feefe2d168fa0c03886cc723
Peter Hansen <hansen.peterd@gmail.com>
2021-09-10 10:13:13 -0500

    [BUGFIX] ignore_inds should not be tuple
    ignore_inds doesn't work unless there is only one index to ignore

--- shap/utils/_general.py
+++ shap/utils/_general.py
@@ -63,7 +63,7 @@ def potential_interactions(shap_values_column, shap_values_matrix):
     """
 
     # ignore inds that are identical to the column 
-    ignore_inds = np.where((shap_values_matrix.values.T - shap_values_column.values).T.std(0) < 1e-8)
+    ignore_inds = np.where((shap_values_matrix.values.T - shap_values_column.values).T.std(0) < 1e-8)[0]
     
     values = shap_values_matrix.values
     X = shap_values_matrix.data
