231f9d14bee42441280688967e885bbdc033b234
kalkairis <kalka.iris@gmail.com>
2021-10-14 10:27:53 +0300

    adding test for beeswarm

---
+++ tests/plots/test_beeswarm.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import pytest
+import shap
+from .utils import explainer # (pytest fixture do not remove) pylint: disable=unused-import
+
+
+@pytest.mark.mpl_image_compare
+def test_beeswarm(explainer): # pylint: disable=redefined-outer-name
+    """ Make sure the violin plot is unchanged.
+    """
+    fig = plt.figure()
+    shap_values = explainer.shap_values(explainer.data)
+    shap.plots.beeswarm(shap_values)
+    plt.tight_layout()
+    return fig
