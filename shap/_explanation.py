9beee49f1cec1aad597ef11096ab56b89d7efa7b
Josh Zwiebel <joshzwiebel@gmail.com>
2022-04-08 12:23:51 -0400

    Fix cohort plot error
    fix an error caused in the cohort bar plot stemming from ambiguous Boolean definition of array

--- shap/_explanation.py
+++ shap/_explanation.py
@@ -323,7 +323,7 @@ class Explanation(metaclass=MetaExplanation):
             pos += 1
 
             # skip over Ellipsis
-            if t == Ellipsis:
+            if np.any(t == Ellipsis):
                 pos += len(self.shape) - len(item)
                 continue
 
