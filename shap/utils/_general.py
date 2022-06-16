e719983db285a8825c663cc02e4c81b310c45692
Alexis Drakopoulos <33128441+alexisdrakopoulos@users.noreply.github.com>
2022-06-16 07:03:14 +0100

    unused

--- shap/utils/_general.py
+++ shap/utils/_general.py
@@ -61,8 +61,6 @@ def potential_interactions(shap_values_column, shap_values_matrix):
 
     # ignore inds that are identical to the column 
     ignore_inds = np.where((shap_values_matrix.values.T - shap_values_column.values).T.std(0) < 1e-8)
-    
-    shap_values_matrix.values
     X = shap_values_matrix.data
 
     if X.shape[0] > 10000:
