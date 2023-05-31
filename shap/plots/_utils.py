2cabb079d4995c3b94b991d63908b9d3ea5d2e6c
dsgibbons <dsgibbons@tuta.io>
2023-06-01 02:02:07 +0930

    chore: make convert_color ruff compliant and add test (#96)
    * chore: make convert_color ruff compliant and add test
    
    * chore: format test

--- shap/plots/_utils.py
+++ shap/plots/_utils.py
@@ -6,17 +6,16 @@ import matplotlib.pyplot as pl
 
 
 def convert_color(color):
-    try:
-        color = pl.get_cmap(color)
-    except Exception:
-        pass
-
     if color == "shap_red":
-        color = colors.red_rgb
-    elif color == "shap_blue":
-        color = colors.blue_rgb
+        return colors.red_rgb
+    if color == "shap_blue":
+        return colors.blue_rgb
 
-    return color
+    try:
+        return pl.get_cmap(color)
+    except ValueError:
+        return color
+
 
 def convert_ordering(ordering, shap_values):
     if issubclass(type(ordering), OpChain):
