6b944cbf6f4ce574ae52656979746c76b14103f3
Jeremy Goh <jeremy_gsk@outlook.com>
2023-06-18 01:09:59 +0800

    clean up tree docstrings (code formatting)

--- shap/explainers/_tree.py
+++ shap/explainers/_tree.py
@@ -76,31 +76,37 @@ class Tree(Explainer):
 
         data : numpy.array or pandas.DataFrame
             The background dataset to use for integrating out features. This argument is optional when
-            feature_perturbation="tree_path_dependent", since in that case we can use the number of training
-            samples that went down each tree path as our background dataset (this is recorded in the model object).
+            ``feature_perturbation="tree_path_dependent"``, since in that case we can use the number of training
+            samples that went down each tree path as our background dataset (this is recorded in the ``model``
+            object).
 
         feature_perturbation : "interventional" (default) or "tree_path_dependent" (default when data=None)
-            Since SHAP values rely on conditional expectations we need to decide how to handle correlated
-            (or otherwise dependent) input features. The "interventional" approach breaks the dependencies between
-            features according to the rules dictated by causal inference (Janzing et al. 2019). Note that the
-            "interventional" option requires a background dataset and its runtime scales linearly with the size
-            of the background dataset you use. Anywhere from 100 to 1000 random background samples are good
-            sizes to use. The "tree_path_dependent" approach is to just follow the trees and use the number
-            of training examples that went down each leaf to represent the background distribution. This approach
-            does not require a background dataset and so is used by default when no background dataset is provided.
+            Since SHAP values rely on conditional expectations, we need to decide how to handle correlated
+            (or otherwise dependent) input features.
+            The "interventional" approach breaks the dependencies between features according to the rules
+            dictated by causal inference (Janzing et al. 2019). Note that the "interventional" option
+            requires a background dataset ``data``, and its runtime scales linearly with the size of the
+            background dataset you use. Anywhere from 100 to 1000 random background samples are good
+            sizes to use.
+            The "tree_path_dependent" approach is to just follow the trees and use the number of training
+            examples that went down each leaf to represent the background distribution. This approach
+            does not require a background dataset, and so is used by default when no background dataset
+            is provided.
 
         model_output : "raw", "probability", "log_loss", or model method name
-            What output of the model should be explained. If "raw", then we explain the raw output of the
-            trees, which varies by model. For regression models, "raw" is the standard output. For binary
-            classification in XGBoost, this is the log odds ratio. If model_output is the name of a supported
-            prediction method on the model object, then we explain the output of that model method name.
-            For example, model_output="predict_proba" explains the result of calling `model.predict_proba`.
+            What output of the model should be explained.
+            If "raw", then we explain the raw output of the trees, which varies by model. For regression models,
+            "raw" is the standard output. For binary classification in XGBoost, this is the log odds ratio.
+            If ``model_output`` is the name of a supported prediction method on the ``model`` object, then we
+            explain the output of that model method name. For example, ``model_output="predict_proba"``
+            explains the result of calling ``model.predict_proba``.
             If "probability", then we explain the output of the model transformed into probability space
-            (note that this means the SHAP values now sum to the probability output of the model). If "log_loss",
-            then we explain the log base e of the model loss function, so that the SHAP values sum up to the
-            log loss of the model for each sample. This is helpful for breaking down model performance by feature.
+            (note that this means the SHAP values now sum to the probability output of the model).
+            If "log_loss", then we explain the log base e of the model loss function, so that the SHAP values
+            sum up to the log loss of the model for each sample. This is helpful for breaking down model
+            performance by feature.
             Currently the "probability" and "log_loss" options are only supported when
-            feature_dependence="independent".
+            ``feature_dependence="interventional"``.
 
         Examples
         --------
