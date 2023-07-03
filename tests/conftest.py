ffc65c93aa6845d28de73bbbe40d33a61c223a01
Connor Tann <71127464+connortann@users.noreply.github.com>
2023-07-03 10:23:24 +0100

    Allow CLI setup of random seed

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,25 +1,21 @@
-import os
-
 import numpy as np
 import pytest
 
 
+def pytest_addoption(parser):
+    parser.addoption("--random-seed", action="store", help="Fix the random seed")
+
+
 @pytest.fixture()
-def random_seed():
+def random_seed(request) -> int:
     """Provides a test-specific random seed for reproducible "fuzz testing".
-
-    By default, each run of the test will use a different seed. Alternatively,
-    the seed can be fixed by setting an environment varable TEST_RANDOM_SEED.
-
-    If the test fails, the random seed used will be displayed in the pytest
-    logs.
 
     Example use in a test:
 
         def test_thing(random_seed):
 
             # Numpy
-            rng = np.random.default_rng(seed=random_seed)
+            rs = np.random.RandomState(seed=random_seed)
             values = rng.integers(...)
 
             # Pytorch
@@ -28,23 +24,32 @@ def random_seed():
             # Tensorflow
             tf.compat.v1.random.set_random_seed(random_seed)
 
+    By default, a new seed is generated on each run of the tests. If a test
+    fails, the random seed used will be displayed in the pytest logs.
+
+    The seed can be fixed by providing a CLI option e.g:
+
+        pytest --random-seed 123
+
+    For numpy usage, note the legacy `RandomState` has stricter version-to-version
+    compatibility guarantees than new-style `default_rng`:
+    https://numpy.org/doc/stable/reference/random/compatibility.html
+
     """
-    try:
-        # If set, use a seed from environment variable
-        seed = int(os.environ['TEST_RANDOM_SEED'])
-    except KeyError:
+    manual_seed = request.config.getoption("--random-seed")
+    if manual_seed is not None:
+        return int(manual_seed)
+    else:
         # Otherwise, create a new seed for each test
-        rng = np.random.default_rng()
-        seed = rng.integers(0, 1000)
-    return seed
+        rs = np.random.RandomState()
+        return rs.randint(0, 1000)
 
 
 @pytest.fixture(autouse=True)
 def global_random_seed():
     """Set the global numpy random seed before each test
 
-    Nb. Tests that use random numbers should instantiate a random number
-    Generator with `np.random.default_rng` rather than use the global numpy
-    random state.
+    Nb. Tests that use random numbers should instantiate a local
+    `np.random.RandomState` rather than use the global numpy random state.
     """
     np.random.seed(0)
