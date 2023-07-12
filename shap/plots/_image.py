c746beb3b9a7d813b4960dfe7c52b4cc4f809efb
Connor Tann <71127464+connortann@users.noreply.github.com>
2023-07-12 22:56:23 +0100

    Delay ipython warning until function called

--- shap/plots/_image.py
+++ shap/plots/_image.py
@@ -1,20 +1,20 @@
 import json
 import random
 import string
-import warnings
 from typing import Optional
 
 import matplotlib.pyplot as pl
 import numpy as np
 from matplotlib.colors import Colormap
 
-from .._explanation import Explanation
-from ..utils import ordinal_str
-
 try:
     from IPython.display import HTML, display
+    have_ipython = True
 except ImportError:
-    warnings.warn("IPython could not be loaded!")
+    have_ipython = False
+
+from .._explanation import Explanation
+from ..utils import ordinal_str
 from ..utils._legacy import kmeans
 from . import colors
 
@@ -190,6 +190,8 @@ def image_to_text(shap_values):
         for each sample
 
     """
+    if not have_ipython:
+        raise ImportError("IPython is required for this function but could not be imported")
 
     if len(shap_values.values.shape) == 5:
         for i in range(shap_values.values.shape[0]):
