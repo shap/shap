4090d41f9b9cbdfbddfc295331a0a4b245dca069
Jeremy Goh <30731072+thatlittleboy@users.noreply.github.com>
2023-07-16 12:34:58 +0800

    import shap.benchmark in tests

--- tests/benchmark/framework.py
+++ tests/benchmark/framework.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-import shap
+import shap.benchmark as benchmark
 
 
 def model(x):
@@ -20,7 +20,7 @@ def test_update():
     perturbation = 'keep'
     scores = {'name': 'test', 'metrics': list(), 'values': dict()}
 
-    shap.benchmark.update(model, X, y, explainer, masker, sort_order, score_function, perturbation, scores)
+    benchmark.update(model, X, y, explainer, masker, sort_order, score_function, perturbation, scores)
 
     metric = perturbation + ' ' + sort_order
 
@@ -31,7 +31,7 @@ def test_get_benchmark():
     """ This is to test the get benchmark function within benchmark/framework
     """
     metrics = {'sort_order': ['positive', 'negative'], 'perturbation': ['keep']}
-    scores = shap.benchmark.get_benchmark(model, X, y, explainer, masker, metrics)
+    scores = benchmark.get_benchmark(model, X, y, explainer, masker, metrics)
 
     expected_metrics = ['keep positive', 'keep negative']
 
@@ -48,5 +48,5 @@ def test_get_metrics():
     expected_metrics1 = set(['keep positive'])
     expected_metrics2 = set(['keep positive', 'keep negative', 'keep absolute'])
 
-    assert set(shap.benchmark.get_metrics(benchmarks, lambda x, y: x.intersection(y))) == expected_metrics1
-    assert set(shap.benchmark.get_metrics(benchmarks, lambda x, y: x.union(y))) == expected_metrics2
+    assert set(benchmark.get_metrics(benchmarks, lambda x, y: x.intersection(y))) == expected_metrics1
+    assert set(benchmark.get_metrics(benchmarks, lambda x, y: x.union(y))) == expected_metrics2
