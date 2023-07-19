8a187f2c095293c3ca64c1c4cb6e1c3618c8591b
connortann <71127464+connortann@users.noreply.github.com>
2023-07-19 23:30:58 +0000

    Rename Coefficent to Coefficient

--- shap/explainers/other/__init__.py
+++ shap/explainers/other/__init__.py
@@ -1,5 +1,13 @@
-from ._coefficent import Coefficient
+import warnings
+
+from ._coefficient import Coefficient
 from ._lime import LimeTabular
 from ._maple import Maple, TreeMaple
 from ._random import Random
 from ._treegain import TreeGain
+
+
+# Deprecated class alias with incorrect spelling
+def Coefficent(*args, **kwargs):  # noqa
+    warnings.warn("Coefficent has been renamed to Coefficient", DeprecationWarning)
+    return Coefficient(*args, **kwargs)
