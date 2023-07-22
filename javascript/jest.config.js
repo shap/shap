8ef0260684c355dec4a02bd90c43511990017fe8
connortann <71127464+connortann@users.noreply.github.com>
2023-07-22 10:29:08 +0100

    Tweak import

--- javascript/jest.config.js
+++ javascript/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   moduleNameMapper: {
     // Fix Jest import of D3. See https://github.com/jestjs/jest/issues/12036
     '^d3$': '<rootDir>/node_modules/d3/dist/d3.min.js',
-    "^d3-(.*)$": `d3-$1/dist/d3-$1`,
+    "^d3-(.*)$": `<rootDir>/node_modules/d3-$1/dist/d3-$1`,
   },
   testEnvironment: 'jsdom'
 };
