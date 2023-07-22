300d25c972a3bbe5eeb13b8fefafcccfd0b86d5e
connortann <71127464+connortann@users.noreply.github.com>
2023-07-22 10:22:09 +0100

    Add fix to D3 Jest import

--- javascript/jest.config.js
+++ javascript/jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
+  moduleNameMapper: {
+    // Fix Jest import of D3. See https://github.com/jestjs/jest/issues/12036
+    '^d3$': '<rootDir>/node_modules/d3/dist/d3.min.js',
+  },
   testEnvironment: 'jsdom'
 };
