8ef6095dd1c3632cd5a4a57de67120104419d1c7
Zakaria Nacer <45372115+Z4K4RI4@users.noreply.github.com>
2023-08-06 23:36:00 +0200

    MAINT: address Matplotlib deprecation warnings in test suite (#3179)
    * Keep features names when using keep_index
    
    * Update shap/explainers/_kernel.py
    
    Co-authored-by: connortann <71127464+connortann@users.noreply.github.com>
    
    * add a docstring
    
    ---------
    
    Co-authored-by: connortann <71127464+connortann@users.noreply.github.com>

--- shap/explainers/_kernel.py
+++ shap/explainers/_kernel.py
@@ -89,9 +89,9 @@ class Kernel(Explainer):
 
         # convert incoming inputs to standardized iml objects
         self.link = convert_to_link(link)
-        self.model = convert_to_model(model)
         self.keep_index = kwargs.get("keep_index", False)
         self.keep_index_ordered = kwargs.get("keep_index_ordered", False)
+        self.model = convert_to_model(model, keep_index=self.keep_index)
         self.data = convert_to_data(data, keep_index=self.keep_index)
         model_null = match_model_to_data(self.model, self.data)
 
