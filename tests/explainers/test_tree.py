c0612e8bd0b76dec6d89146a3a44e1f0e5ebd9c5
Connor Tann <71127464+connortann@users.noreply.github.com>
2023-09-14 22:18:49 +0100

    Add test to reproduce #3261

--- tests/explainers/test_tree.py
+++ tests/explainers/test_tree.py
@@ -139,11 +139,12 @@ def _validate_shap_values(model, x_test):
     )
 
 
-def test_ngboost():
+@pytest.mark.parametrize("col_sample", [1.0, 0.9])
+def test_ngboost(col_sample: float):
     ngboost = pytest.importorskip("ngboost")
 
     X, y = shap.datasets.california(n_points=500)
-    model = ngboost.NGBRegressor(n_estimators=20).fit(X, y)
+    model = ngboost.NGBRegressor(n_estimators=20, col_sample=col_sample).fit(X, y)
     predicted = model.predict(X)
 
     # explain the model's predictions using SHAP values
