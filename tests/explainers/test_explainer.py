2399c25e41e2da1083a88bb9d75aecc85a356c37
Jeremy Goh <30731072+thatlittleboy@users.noreply.github.com>
2023-10-07 17:05:48 +0800

    refactor: raise error during init if masker is None for PermutationExplainer

--- tests/explainers/test_explainer.py
+++ tests/explainers/test_explainer.py
@@ -2,8 +2,28 @@
 """
 
 import pytest
+import sklearn
 
 import shap
+
+
+def test_explainer_to_permutationexplainer():
+    """Checks that Explainer maps to PermutationExplainer as expected."""
+
+    X_train, X_test, y_train, _ = sklearn.model_selection.train_test_split(*shap.datasets.adult(), test_size=0.1, random_state=0)
+    lr = sklearn.linear_model.LogisticRegression(solver="liblinear")
+    lr.fit(X_train, y_train)
+
+    explainer = shap.Explainer(lr.predict_proba, masker=X_train)
+    assert isinstance(explainer, shap.PermutationExplainer)
+
+    # ensures a proper error message is raised if a masker is not provided (GH #3310)
+    with pytest.raises(
+        ValueError,
+        match=r"masker cannot be None",
+    ):
+        explainer = shap.Explainer(lr.predict_proba)
+        _ = explainer(X_test)
 
 
 def test_wrapping_for_text_to_text_teacher_forcing_model():
