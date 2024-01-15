10912f1783c01b8726bbb2bed45f6b95fbd756d3
Tobias Pitters <tobias.pitters@gmail.com>
2024-01-15 23:14:42 +0100

    do stuff

--- scripts/run_notebooks_timeouts.py
+++ scripts/run_notebooks_timeouts.py
@@ -47,13 +47,13 @@ def main():
 
                 convert_notebook_to_python(notebook_path, python_script_path)
 
-                error_code, execution_time = execute_python_script(python_script_path, timeout_seconds=TIMEOUT)
+                # error_code, execution_time = execute_python_script(python_script_path, timeout_seconds=TIMEOUT)
 
-                if error_code == -1:
-                    print(f"Execution of {notebook_path} timed out after {TIMEOUT} seconds.")
-                    error_notebooks.append((notebook_path, -1, None))
-                elif error_code != 0:
-                    error_notebooks.append((notebook_path, error_code, execution_time))
+                # if error_code == -1:
+                #     print(f"Execution of {notebook_path} timed out after {TIMEOUT} seconds.")
+                #     error_notebooks.append((notebook_path, -1, None))
+                # elif error_code != 0:
+                #     error_notebooks.append((notebook_path, error_code, execution_time))
 
     if error_notebooks:
         error_thrown = [error_code for _, error_code, _ in error_notebooks if error_code != -1]
