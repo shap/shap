2c40393bdbb837d1bd6739f56d3a92244de8ff02
Tobias Pitters <tobias.pitters@gmail.com>
2024-01-23 21:31:14 +0100

    make paths relative to notebook dir

--- scripts/run_notebooks_timeouts.py
+++ scripts/run_notebooks_timeouts.py
@@ -7,65 +7,65 @@ from nbconvert.preprocessors import CellExecutionError, ExecutePreprocessor
 TIMEOUT = 20  # seconds
 
 allow_to_fail = [
-    Path("notebooks/tabular_examples/tree_based_models/tree_shap_paper/Tree SHAP in Python.ipynb"),
-    Path("notebooks/api_examples/plots/decision_plot.ipynb"),
-    Path("notebooks/image_examples/image_classification/Image Multi Class.ipynb"),
-    Path("notebooks/tabular_examples/model_agnostic/Multioutput Regression SHAP.ipynb"),
-    Path("notebooks/tabular_examples/neural_networks/Census income classification with Keras.ipynb"),
-    Path("notebooks/api_examples/explainers/GPUTree.ipynb"),
-    Path("notebooks/genomic_examples/DeepExplainer Genomics Example.ipynb"),
-    Path("notebooks/tabular_examples/tree_based_models/Perfomance Comparison.ipynb"),
-    Path("notebooks/tabular_examples/tree_based_models/tree_shap_paper/Figure 7 - Airline Tweet Sentiment Analysis.ipynb"),
-    Path("notebooks/overviews/Be careful when interpreting predictive models in search of causal insights.ipynb"),
-    Path("notebooks/benchmarks/text/Text Emotion Multiclass Classification Benchmark Demo.ipynb"),
-    Path("notebooks/tabular_examples/tree_based_models/tree_shap_paper/Figure 6 - Supervised Clustering R-squared.ipynb"),
-    Path("notebooks/overviews/Explaining quantitative measures of fairness.ipynb"),
-    Path("notebooks/image_examples/image_captioning/Image Captioning using Open Source.ipynb"),
-    Path("notebooks/tabular_examples/tree_based_models/tree_shap_paper/Performance comparison.ipynb"),
-    Path("notebooks/tabular_examples/tree_based_models/League of Legends Win Prediction with XGBoost.ipynb"),
-    Path("notebooks/overviews/An introduction to explainable AI with Shapley values.ipynb"),
-    Path("notebooks/image_examples/image_captioning/Image Captioning using Azure Cognitive Services.ipynb"),
-    Path("notebooks/tabular_examples/tree_based_models/tree_shap_paper/Figures 8-11 NHANES I Survival Model.ipynb"),
-    Path("notebooks/benchmarks/others/Benchmark Debug Mode.ipynb"),
-    Path("notebooks/tabular_examples/tree_based_models/NHANES I Survival Model.ipynb"),
-    Path("notebooks/benchmarks/text/Abstractive Summarization Benchmark Demo.ipynb"),
-    Path("notebooks/tabular_examples/tree_based_models/tree_shap_paper/Performance comparison copy.ipynb"),
-    Path("notebooks/tabular_examples/tree_based_models/tree_shap_paper/Figures 8-11 NHANES I Survival Model-Copy1.ipynb"),
+    Path("tabular_examples/tree_based_models/tree_shap_paper/Tree SHAP in Python.ipynb"),
+    Path("api_examples/plots/decision_plot.ipynb"),
+    Path("image_examples/image_classification/Image Multi Class.ipynb"),
+    Path("tabular_examples/model_agnostic/Multioutput Regression SHAP.ipynb"),
+    Path("tabular_examples/neural_networks/Census income classification with Keras.ipynb"),
+    Path("api_examples/explainers/GPUTree.ipynb"),
+    Path("genomic_examples/DeepExplainer Genomics Example.ipynb"),
+    Path("tabular_examples/tree_based_models/Perfomance Comparison.ipynb"),
+    Path("tabular_examples/tree_based_models/tree_shap_paper/Figure 7 - Airline Tweet Sentiment Analysis.ipynb"),
+    Path("overviews/Be careful when interpreting predictive models in search of causal insights.ipynb"),
+    Path("benchmarks/text/Text Emotion Multiclass Classification Benchmark Demo.ipynb"),
+    Path("tabular_examples/tree_based_models/tree_shap_paper/Figure 6 - Supervised Clustering R-squared.ipynb"),
+    Path("overviews/Explaining quantitative measures of fairness.ipynb"),
+    Path("image_examples/image_captioning/Image Captioning using Open Source.ipynb"),
+    Path("tabular_examples/tree_based_models/tree_shap_paper/Performance comparison.ipynb"),
+    Path("tabular_examples/tree_based_models/League of Legends Win Prediction with XGBoost.ipynb"),
+    Path("overviews/An introduction to explainable AI with Shapley values.ipynb"),
+    Path("image_examples/image_captioning/Image Captioning using Azure Cognitive Services.ipynb"),
+    Path("tabular_examples/tree_based_models/tree_shap_paper/Figures 8-11 NHANES I Survival Model.ipynb"),
+    Path("benchmarks/others/Benchmark Debug Mode.ipynb"),
+    Path("tabular_examples/tree_based_models/NHANES I Survival Model.ipynb"),
+    Path("benchmarks/text/Abstractive Summarization Benchmark Demo.ipynb"),
+    Path("tabular_examples/tree_based_models/tree_shap_paper/Performance comparison copy.ipynb"),
+    Path("tabular_examples/tree_based_models/tree_shap_paper/Figures 8-11 NHANES I Survival Model-Copy1.ipynb"),
 ]
 
 allow_to_timeout = [
-    Path("notebooks/tabular_examples/model_agnostic/Census income classification with scikit-learn.ipynb"),
-    Path("notebooks/image_examples/image_classification/Explain MobilenetV2 using the Partition explainer (PyTorch).ipynb"),
-    Path("notebooks/text_examples/sentiment_analysis/Emotion classification multiclass example.ipynb"),
-    Path("notebooks/api_examples/plots/bar.ipynb"),
-    Path("notebooks/text_examples/language_modelling/Language Modeling Explanation Demo.ipynb"),
-    Path("notebooks/image_examples/image_classification/Explain an Intermediate Layer of VGG16 on ImageNet (PyTorch).ipynb"),
-    Path("notebooks/text_examples/sentiment_analysis/Keras LSTM for IMDB Sentiment Classification.ipynb"),
-    Path("notebooks/text_examples/sentiment_analysis/Positive vs. Negative Sentiment Classification.ipynb"),
-    Path("notebooks/image_examples/image_classification/Multi-class ResNet50 on ImageNet (TensorFlow).ipynb"),
-    Path("notebooks/tabular_examples/tree_based_models/Fitting a Linear Simulation with XGBoost.ipynb"),
-    Path("notebooks/text_examples/question_answering/Explaining a Question Answering Transformers Model.ipynb"),
-    Path("notebooks/text_examples/sentiment_analysis/Using custom functions and tokenizers.ipynb"),
-    Path("notebooks/text_examples/translation/Machine Translation Explanations.ipynb"),
-    Path("notebooks/image_examples/image_classification/Explain an Intermediate Layer of VGG16 on ImageNet.ipynb"),
-    Path("notebooks/image_examples/image_classification/Multi-class ResNet50 on ImageNet (TensorFlow)-checkpoint.ipynb"),
-    Path("notebooks/api_examples/plots/text.ipynb"),
-    Path("notebooks/image_examples/image_classification/Explain ResNet50 using the Partition explainer.ipynb"),
-    Path("notebooks/benchmarks/tabular/Tabular Prediction Benchmark Demo.ipynb"),
-    Path("notebooks/text_examples/text_generation/Open Ended GPT2 Text Generation Explanations.ipynb"),
-    Path("notebooks/image_examples/image_classification/Front Page DeepExplainer MNIST Example.ipynb"),
-    Path("notebooks/tabular_examples/tree_based_models/Census income classification with XGBoost.ipynb"),
-    Path("notebooks/api_examples/plots/waterfall.ipynb"),
-    Path("notebooks/benchmarks/text/Machine Translation Benchmark Demo.ipynb"),
-    Path("notebooks/text_examples/summarization/Abstractive Summarization Explanation Demo.ipynb"),
-    Path("notebooks/api_examples/plots/beeswarm.ipynb"),
-    Path("notebooks/api_examples/plots/image.ipynb"),
-    Path("notebooks/benchmarks/tabular/Benchmark XGBoost explanations.ipynb"),
-    Path("notebooks/benchmarks/image/Image Multiclass Classification Benchmark Demo.ipynb"),
-    Path("notebooks/api_examples/plots/scatter.ipynb"),
-    Path("notebooks/text_examples/text_entailment/Textual Entailment Explanation Demo.ipynb"),
-    Path("notebooks/image_examples/image_classification/Multi-input Gradient Explainer MNIST Example.ipynb"),
-    Path("notebooks/image_examples/image_classification/PyTorch Deep Explainer MNIST example.ipynb"),
+    Path("tabular_examples/model_agnostic/Census income classification with scikit-learn.ipynb"),
+    Path("image_examples/image_classification/Explain MobilenetV2 using the Partition explainer (PyTorch).ipynb"),
+    Path("text_examples/sentiment_analysis/Emotion classification multiclass example.ipynb"),
+    Path("api_examples/plots/bar.ipynb"),
+    Path("text_examples/language_modelling/Language Modeling Explanation Demo.ipynb"),
+    Path("image_examples/image_classification/Explain an Intermediate Layer of VGG16 on ImageNet (PyTorch).ipynb"),
+    Path("text_examples/sentiment_analysis/Keras LSTM for IMDB Sentiment Classification.ipynb"),
+    Path("text_examples/sentiment_analysis/Positive vs. Negative Sentiment Classification.ipynb"),
+    Path("image_examples/image_classification/Multi-class ResNet50 on ImageNet (TensorFlow).ipynb"),
+    Path("tabular_examples/tree_based_models/Fitting a Linear Simulation with XGBoost.ipynb"),
+    Path("text_examples/question_answering/Explaining a Question Answering Transformers Model.ipynb"),
+    Path("text_examples/sentiment_analysis/Using custom functions and tokenizers.ipynb"),
+    Path("text_examples/translation/Machine Translation Explanations.ipynb"),
+    Path("image_examples/image_classification/Explain an Intermediate Layer of VGG16 on ImageNet.ipynb"),
+    Path("image_examples/image_classification/Multi-class ResNet50 on ImageNet (TensorFlow)-checkpoint.ipynb"),
+    Path("api_examples/plots/text.ipynb"),
+    Path("image_examples/image_classification/Explain ResNet50 using the Partition explainer.ipynb"),
+    Path("benchmarks/tabular/Tabular Prediction Benchmark Demo.ipynb"),
+    Path("text_examples/text_generation/Open Ended GPT2 Text Generation Explanations.ipynb"),
+    Path("image_examples/image_classification/Front Page DeepExplainer MNIST Example.ipynb"),
+    Path("tabular_examples/tree_based_models/Census income classification with XGBoost.ipynb"),
+    Path("api_examples/plots/waterfall.ipynb"),
+    Path("benchmarks/text/Machine Translation Benchmark Demo.ipynb"),
+    Path("text_examples/summarization/Abstractive Summarization Explanation Demo.ipynb"),
+    Path("api_examples/plots/beeswarm.ipynb"),
+    Path("api_examples/plots/image.ipynb"),
+    Path("benchmarks/tabular/Benchmark XGBoost explanations.ipynb"),
+    Path("benchmarks/image/Image Multiclass Classification Benchmark Demo.ipynb"),
+    Path("api_examples/plots/scatter.ipynb"),
+    Path("text_examples/text_entailment/Textual Entailment Explanation Demo.ipynb"),
+    Path("image_examples/image_classification/Multi-input Gradient Explainer MNIST Example.ipynb"),
+    Path("image_examples/image_classification/PyTorch Deep Explainer MNIST example.ipynb"),
 ]
 
 
@@ -78,17 +78,18 @@ def main():
     timeout_notebooks = []
     notebooks_to_run = set(notebooks_directory.rglob('*.ipynb')) - set(allow_to_fail) - set(allow_to_timeout)
     for file_path in notebooks_to_run:
-        with open(file_path) as f:
+        notebook_path = notebooks_directory / file_path
+        with open(notebook_path) as f:
             nb = nbformat.read(f, as_version=4)
         start_time = time.time()
         try:
-            ep.preprocess(nb, {'metadata': {'path': str(file_path.parent)}})
-            print(f"Executed notebook {file_path} in {time.time() - start_time:.2f} seconds.")
+            ep.preprocess(nb, {'metadata': {'path': str(notebook_path.parent)}})
+            print(f"Executed notebook {notebook_path} in {time.time() - start_time:.2f} seconds.")
         except CellExecutionError:
-            error_notebooks.append(file_path)
+            error_notebooks.append(notebook_path)
         except TimeoutError:
-            print(f"Execution of {file_path} timed out after {TIMEOUT} seconds.")
-            timeout_notebooks.append(file_path)
+            print(f"Execution of {notebook_path} timed out after {TIMEOUT} seconds.")
+            timeout_notebooks.append(notebook_path)
 
     if len(error_notebooks) > 0:
         print(f"Notebooks failed with error codes: {', '.join([str(nb) for nb in error_notebooks])}")
