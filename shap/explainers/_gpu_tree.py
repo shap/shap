c4b3821ad6689426da906f793476b04fe463e2a0
Connor Tann <71127464+connortann@users.noreply.github.com>
2024-02-19 14:17:51 +0000

    Remove duplicate init docstring

--- shap/explainers/_gpu_tree.py
+++ shap/explainers/_gpu_tree.py
@@ -15,48 +15,6 @@ class GPUTreeExplainer(TreeExplainer):
     Experimental GPU accelerated version of TreeExplainer. Currently requires source build with
     cuda available and 'CUDA_PATH' environment variable defined.
 
-    Parameters
-    ----------
-    model : model object
-        The tree based machine learning model that we want to explain. XGBoost, LightGBM,
-        CatBoost, Pyspark and most tree-based scikit-learn models are supported.
-
-    data : numpy.array or pandas.DataFrame
-        The background dataset to use for integrating out features. This argument is optional when
-        ``feature_perturbation="tree_path_dependent"``, since in that case we can use the number of training
-        samples that went down each tree path as our background dataset (this is recorded in the ``model``
-        object).
-
-    feature_perturbation : "interventional" (default) or "tree_path_dependent" (default when data=None)
-        Since SHAP values rely on conditional expectations, we need to decide how to handle correlated
-        (or otherwise dependent) input features.
-        The "interventional" approach breaks the dependencies between features according to the rules
-        dictated by causal inference (Janzing et al. 2019). Note that the "interventional" option
-        requires a background dataset ``data``, and its runtime scales linearly with the size of the
-        background dataset you use. Anywhere from 100 to 1000 random background samples are good
-        sizes to use.
-        The "tree_path_dependent" approach is to just follow the trees and use the number of training
-        examples that went down each leaf to represent the background distribution. This approach
-        does not require a background dataset, and so is used by default when no background dataset
-        is provided.
-
-    model_output : "raw", "probability", "log_loss", or model method name
-        What output of the model should be explained.
-
-        * If "raw", then we explain the raw output of the trees, which varies by model. For regression models,
-          "raw" is the standard output. For binary classification in XGBoost, this is the log odds ratio.
-        * If "probability", then we explain the output of the model transformed into probability space
-          (note that this means the SHAP values now sum to the probability output of the model).
-        * If "log_loss", then we explain the log base e of the model loss function, so that the SHAP values
-          sum up to the log loss of the model for each sample. This is helpful for breaking down model
-          performance by feature.
-        * If ``model_output`` is the name of a supported prediction method on the ``model`` object, then we
-          explain the output of that model method name. For example, ``model_output="predict_proba"``
-          explains the result of calling ``model.predict_proba``.
-
-        Currently the "probability" and "log_loss" options are only supported when
-        ``feature_perturbation="interventional"``.
-
     Examples
     --------
     See `GPUTree explainer examples <https://shap.readthedocs.io/en/latest/api_examples/explainers/GPUTreeExplainer.html>`_
