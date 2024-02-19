0d498ddd0b042f780338097aeba9b5f889abbb19
Connor Tann <71127464+connortann@users.noreply.github.com>
2024-02-19 14:31:58 +0000

    Remove deprecated misspelling

--- shap/explainers/other/__init__.py
+++ shap/explainers/other/__init__.py
@@ -1,4 +1,3 @@
-import warnings
 
 from ._coefficient import Coefficient
 from ._lime import LimeTabular
@@ -14,13 +13,3 @@ __all__ = [
     "Random",
     "TreeGain",
 ]
-
-
-# Deprecated class alias with incorrect spelling
-def Coefficent(*args, **kwargs):  # noqa
-    warnings.warn(
-        "Coefficent has been renamed to Coefficient. "
-        "The former is deprecated and will be removed in shap 0.45.",
-        DeprecationWarning
-    )
-    return Coefficient(*args, **kwargs)
