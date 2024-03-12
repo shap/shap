00b017c37c1c142dd63ec153964d9d4d6ccf9ff1
costrau <christoph.ostrau@hsbi.de>
2024-03-12 19:04:12 +0100

    Update is_transformers_lm

--- shap/utils/transformers.py
+++ shap/utils/transformers.py
@@ -85,8 +85,13 @@ SENTENCEPIECE_TOKENIZERS = [
 def is_transformers_lm(model):
     """ Check if the given model object is a huggingface transformers language model.
     """
-    return (safe_isinstance(model, "transformers.PreTrainedModel") or safe_isinstance(model, "transformers.TFPreTrainedModel")) and \
-        safe_isinstance(model, MODELS_FOR_SEQ_TO_SEQ_CAUSAL_LM + MODELS_FOR_CAUSAL_LM)
+    if safe_isinstance(
+            model, "transformers.PreTrainedModel") or safe_isinstance(
+            model, "transformers.TFPreTrainedModel"):
+        from transformers import MODEL_FOR_SEQ_TO_SEQ_CAUSAL_LM_MAPPING, MODEL_FOR_CAUSAL_LM_MAPPING
+        return type(model) in MODEL_FOR_SEQ_TO_SEQ_CAUSAL_LM_MAPPING.values() or type(
+            model) in MODEL_FOR_CAUSAL_LM_MAPPING.values()
+    return False
 
 def parse_prefix_suffix_for_tokenizer(tokenizer):
     """ Set prefix and suffix tokens based on null tokens.
