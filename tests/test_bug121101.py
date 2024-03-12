c37b56c1518e3e57f395e0455194e32a40d725b1
Connor Tann <71127464+connortann@users.noreply.github.com>
2024-03-12 10:19:04 +0000

    Dummy run to repro

---
+++ tests/test_bug121101.py
@@ -0,0 +1,10 @@
+import time
+
+import torch
+from sklearn.datasets import fetch_california_housing
+
+
+def test_something():
+    X, y = fetch_california_housing(return_X_y=True)
+    torch.tensor(X)
+    time.sleep(3)
