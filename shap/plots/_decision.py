86883ccde547ff28ec31173ca0f34c6010781b49
Tobias Pitters <tobias.pitters@gmail.com>
2024-06-27 23:57:20 +0200

    test decision plot

--- shap/plots/_decision.py
+++ shap/plots/_decision.py
@@ -162,6 +162,7 @@ def __decision_plot_matplotlib(
         pl.show()
 
 
+
 class DecisionPlotResult:
     """The optional return value of decision_plot.
 
@@ -582,6 +583,7 @@ def multioutput_decision(base_values, shap_values, row_index, **kwargs) -> Union
         Returns a DecisionPlotResult object if `return_objects=True`. Returns `None` otherwise (the default).
 
     """
+    # todo: adjust to breaking changes made in #3318
     if not (isinstance(base_values, list) and isinstance(shap_values, list)):
         raise ValueError("The base_values and shap_values args expect lists.")
 
