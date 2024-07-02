65fa6b08bec37e5ca35244345a2d36afa8ce29d6
connortann <71127464+connortann@users.noreply.github.com>
2024-07-02 15:51:27 +0100

    Switch to use spmatrix for sparse matrices

--- shap/utils/_types.py
+++ shap/utils/_types.py
@@ -5,5 +5,5 @@ import pandas as pd
 import scipy.sparse
 
 # TODO: use TypeAlias (when we drop python 3.9)
-_ArrayLike = Union[np.ndarray, pd.DataFrame, pd.Series, list, scipy.sparse.csr_matrix]
-_ArrayT = TypeVar("_ArrayT", np.ndarray, pd.DataFrame, pd.Series, scipy.sparse.csr_matrix, list)
+_ArrayLike = Union[np.ndarray, pd.DataFrame, pd.Series, list, scipy.sparse.spmatrix]
+_ArrayT = TypeVar("_ArrayT", np.ndarray, pd.DataFrame, pd.Series, scipy.sparse.spmatrix, list)
