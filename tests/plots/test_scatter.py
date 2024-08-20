98f0b61c0ddc41595c765f79d9cccdeb0cc608a0
David Born <dborn56@gmail.com>
2024-08-20 04:03:50 -0400

    #3135: Adds categorical support for shap.plots.scatter (#3706)
    * #3135: Adds categorical support for shap.plots.scatter
    
    * Adds encoding in response to comment on #3135
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fixes conditional indentation
    
    * Revert changes to unaffected baseline images
    
    * Rename baseline image
    
    * Fixup formatting
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: connortann <71127464+connortann@users.noreply.github.com>

--- tests/plots/test_scatter.py
+++ tests/plots/test_scatter.py
@@ -66,3 +66,38 @@ def test_scatter_custom(explainer):
     fig = plt.gcf()
     plt.tight_layout()
     return fig
+
+
+@pytest.fixture()
+def categorical_explanation():
+    """Adopted from explainer in conftest.py but using a categorical input."""
+    xgboost = pytest.importorskip("xgboost")
+    # get a dataset on income prediction
+    X, y = shap.datasets.diabetes()
+
+    # Swap the input data from a "float-category"
+    # To a string category in order to test scatter with
+    # true non-float-castable category values
+    X.loc[X["sex"] < 0, "sex"] = 0
+    X.loc[X["sex"] > 0, "sex"] = 1
+    X["sex"] = X["sex"].map({1: "Male", 0: "Female"}).astype("category")
+
+    # train an XGBoost model (but any other model type would also work)
+    model = xgboost.XGBRegressor(random_state=0, enable_categorical=True, max_cat_to_onehot=1, base_score=0.5)
+    model.fit(X, y)
+    # build an Exact explainer and explain the model predictions on the given dataset
+    # We aren't providing masker directly because there appears
+    # to be an error with string categories when using masker like this
+    # TODO: Investigate the error when this line is `return shap.Explainer(model, X)``
+    explainer = shap.TreeExplainer(model)
+    shap_values = explainer(X)
+    return shap_values
+
+
+@pytest.mark.mpl_image_compare(tolerance=3)
+def test_scatter_categorical(categorical_explanation):
+    """Test the scatter plot with categorical data. See GH #3135"""
+    fig, ax = plt.subplots()
+    shap.plots.scatter(categorical_explanation[:, "sex"], ax=ax, show=False)
+    plt.tight_layout()
+    return fig
