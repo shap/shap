1635f7c9291e2e5d654f66a9d19db2d5fb064588
connortann <71127464+connortann@users.noreply.github.com>
2024-09-09 17:02:43 +0100

    Switch to pinning subset of bounds

---
+++ scripts/extract_lower_bounds.py
@@ -0,0 +1,36 @@
+"""Utility script to print lowest supported versions of dependencies"""
+
+import tomllib
+from packaging.requirements import Requirement
+
+
+def parse_lower_bounds(dependencies: list[str]) -> dict[str, str]:
+    """Extract any declared ">=" lower bounds from a list of dependencies."""
+    lower_bounds = {}
+    for dep in dependencies:
+        req = Requirement(dep)
+        for spec in req.specifier:
+            if spec.operator == ">=":
+                lower_bounds[req.name] = spec.version
+    return lower_bounds
+
+
+def main():
+    # Parse all declared lower bound dependencies from pyproject.toml
+    with open("pyproject.toml", "rb") as f:
+        data = tomllib.load(f)
+
+    # Core dependencies
+    bounds = parse_lower_bounds(data["project"]["dependencies"])
+    # Optional dependencies
+    optional_deps = data["project"]["optional-dependencies"]
+    for group in ["test", "plots"]:
+        bounds.update(parse_lower_bounds(optional_deps[group]))
+
+    # Print out these dependencies as a list of pinned versions
+    for dep, version in bounds.items():
+        print(f"{dep}=={version}")
+
+
+if __name__ == "__main__":
+    main()
