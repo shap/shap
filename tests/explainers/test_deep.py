3b8c75e3683846e24f9aa607f181d0ba874d9e13
Connor Tann <71127464+connortann@users.noreply.github.com>
2025-01-11 17:01:48 +0000

    Revert "Try disable limit for memory alloc"
    This reverts commit fedca50da282a165772073deab05bf9b62ef9b2d.

--- tests/explainers/test_deep.py
+++ tests/explainers/test_deep.py
@@ -1,5 +1,7 @@
 """Tests for the Deep explainer."""
 
+import platform
+
 import numpy as np
 import pandas as pd
 import pytest
@@ -241,14 +243,12 @@ def test_tf_keras_imdb_lstm(random_seed):
     np.testing.assert_allclose(sums, diff, atol=1e-02), "Sum of SHAP values does not match difference!"
 
 
-def test_tf_deep_imbdb_transformers(monkeypatch):
+@pytest.mark.skipif(platform.system() == "Darwin", reason="Skipping on MacOS due to memory error on GH runners")
+def test_tf_deep_imbdb_transformers():
     # GH 3522
     transformers = pytest.importorskip("transformers")
 
     from shap import models
-
-    # Disable upper limit for memory allocations (see GH #3929)
-    monkeypatch.setenv("PYTORCH_MPS_HIGH_WATERMARK_RATIO", "0.0")
 
     # data from datasets imdb dataset
     short_data = ["I lov", "Worth", "its a", "STAR ", "First", "I had", "Isaac", "It ac", "Techn", "Hones"]
