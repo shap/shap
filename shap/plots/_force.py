f50cebeebd6dd1c081b7cfb3d6c1a2e038684148
Mahaveerjain-18 <mahaveerjain1098765@gmail.com>
2026-04-02 14:51:18 +0530

    Fix matplotlib force_plot truncation using tight_layout when show=True

--- shap/plots/_force.py
+++ shap/plots/_force.py
@@ -516,7 +516,11 @@ class AdditiveForceVisualizer(BaseVisualizer):
 </script>"""
 
     def matplotlib(self, figsize, show, text_rotation, min_perc=0.05):
-        fig = draw_additive_plot(self.data, figsize=figsize, show=show, text_rotation=text_rotation, min_perc=min_perc)
+        fig = draw_additive_plot(
+            self.data,figsize=figsize,show=show,text_rotation=text_rotation,min_perc=min_perc
+        )
+        if fig is not None and show:
+            fig.tight_layout()
 
         return fig
 
