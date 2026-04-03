93eccf3907d9ff780ae84d675e67364bd7be010c
Sowmyadevalla2005 <sowmyadevalla49@gmail.com>
2026-04-03 23:54:07 +0530

    Add unit test for DimensionError in force_plot

---
+++ tests/plots/test_force_plot.py
@@ -0,0 +1,13 @@
+import pytest
+import numpy as np
+import shap
+from shap.utils._exceptions import DimensionError
+
+
+def test_force_plot_dimension_mismatch():
+    base_value = 0.5
+    shap_values = np.array([1, 2, 3])
+    features = np.array([1, 2])  # mismatch
+
+    with pytest.raises(DimensionError):
+        shap.force_plot(base_value, shap_values, features, matplotlib=True)
