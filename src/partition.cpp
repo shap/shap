6b3f5351a1dc4526d8cf7e6d7a649c42e2e30c2a
Zhihao Dai <zhihao.dai@eng.ox.ac.uk>
2026-04-03 02:02:57 +0100

    Add 2d `lower_credit`
    Co-authored-by: Zhihao Dai <zhihao.dai@eng.ox.ac.uk>

--- src/partition.cpp
+++ src/partition.cpp
@@ -41,6 +41,44 @@ namespace partition
         lower_credit_1d(ri, right_value, M, values, clustering);
     }
 
+    void lower_credit_2d(
+        int i,
+        int M,
+        int prev_i,
+        double factor,
+        nb::ndarray<nb::numpy, double> values,
+        nb::ndarray<nb::numpy, double, nb::ndim<2>> clustering)
+    {
+        auto v = values.view<double, nb::ndim<2>>();
+        for (int j = 0; j < v.shape(1); ++j)
+        {
+            v(i, j) += factor * v(prev_i, j);
+        }
+        if (i < M)
+        {
+            return;
+        }
+
+        auto c = clustering.view<double, nb::ndim<2>>();
+
+        const int li = static_cast<int>(c(i - M, 0));
+        const int ri = static_cast<int>(c(i - M, 1));
+        const int group_size = static_cast<int>(c(i - M, 3));
+        const int lsize = li >= M ? static_cast<int>(c(li - M, 3)) : 1;
+        const int rsize = ri >= M ? static_cast<int>(c(ri - M, 3)) : 1;
+
+        if (lsize + rsize != group_size)
+        {
+            throw nb::value_error("left and right cluster sizes do not match parent group size");
+        }
+
+        const double left_factor = static_cast<double>(lsize) / static_cast<double>(group_size);
+        const double right_factor = static_cast<double>(rsize) / static_cast<double>(group_size);
+
+        lower_credit_2d(li, M, i, left_factor, values, clustering);
+        lower_credit_2d(ri, M, i, right_factor, values, clustering);
+    }
+
     void lower_credit(
         int i,
         int M,
@@ -50,6 +88,10 @@ namespace partition
         if (values.ndim() == 1)
         {
             lower_credit_1d(i, 0, M, values, clustering);
+        }
+        else if (values.ndim() == 2)
+        {
+            lower_credit_2d(i, M, i, 0.0, values, clustering);
         }
         else
         {
