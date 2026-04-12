ca78cdd36c74ce3122b12724ecbfc28abacd0efe
DashratRajpurohit <dashrat.rajpurohit.cg@gamil.com>
2026-04-12 16:35:12 +0530

    fix: use os.path.join in make_dir to prevent path concatenation error

--- shap/utils/image.py
+++ shap/utils/image.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 from textwrap import wrap
 
 import cv2
@@ -40,7 +41,11 @@ def make_dir(path):
         if os.listdir(path):
             # if exists, empty directory
             for file in os.listdir(path):
-                os.remove(path + file)
+                file_path = os.path.join(path, file)
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.remove(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
 
 
 def add_sample_images(path):
