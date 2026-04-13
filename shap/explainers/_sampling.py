9294e91b458bca2a04745b8167366fff6e9c0237
Likhith K Raj <likhithdrait.edu.in@gmail.com>
2026-04-13 16:18:31 +0530

    DOCS: add example link to SamplingExplainer docstring

--- shap/explainers/_sampling.py
+++ shap/explainers/_sampling.py
@@ -40,6 +40,9 @@ class SamplingExplainer(KernelExplainer):
         KernelExplainer, this data can be the whole training set, even if that is a large set. This
         is because SamplingExplainer only samples from this background dataset.
 
+    Examples
+    --------
+    See `SHAP example notebooks <https://shap.readthedocs.io/en/latest/api_examples/>`_
     """
 
     def __init__(
