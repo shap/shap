2216cd5d53ccfb69b01a7055a15d11cc0ac57e54
Tobias Pitters <tobias.pitters@gmail.com>
2026-04-14 17:14:27 +0200

    ignore mypy for stubs

--- shap/cutils/cutils.cpp
+++ shap/cutils/cutils.cpp
@@ -7,7 +7,7 @@ namespace nb = nanobind;
 
 NB_MODULE(_cutils, m)
 {
-    m.def("compute_grey_code_row_values", &compute_grey_code_row_values_1d);
-    m.def("compute_grey_code_row_values", &compute_grey_code_row_values_2d);
-    m.def("compute_exp_val", &compute_exp_val);
+    m.def("compute_grey_code_row_values", &compute_grey_code_row_values_1d, "row_values"_a, "mask"_a, "inds"_a, "outputs"_a, "shapley_coeff"_a, "extended_delta_indexes"_a, "noop_code"_a, "Compute the row values for the grey code algorithm in 1D");
+    m.def("compute_grey_code_row_values", &compute_grey_code_row_values_2d, "row_values"_a, "mask"_a, "inds"_a, "outputs"_a, "shapley_coeff"_a, "extended_delta_indexes"_a, "noop_code"_a, "Compute the row values for the grey code algorithm in 2D");
+    m.def("compute_exp_val", &compute_exp_val, "nsamples_run"_a, "nsamples_added"_a, "D"_a, "N"_a, "weights"_a, "y"_a, "ey"_a, "Compute the expected value for the kernel explainer algorithm");
 }
