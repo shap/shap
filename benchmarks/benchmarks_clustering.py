759821e49285b6771af20627ff96518facb7daf0
Arun Thakur <bedupako12mas@gmail.com>
2026-04-18 19:41:51 +0530

    BENCH: add benchmark for reverse_window_score_gain

--- benchmarks/benchmarks_clustering.py
+++ benchmarks/benchmarks_clustering.py
@@ -29,6 +29,13 @@ try:
 except ImportError:
     _HAS_REVERSE_WINDOW = False
 
+try:
+    from shap._cutils import reverse_window_score_gain as _cpp_reverse_window_score_gain
+
+    _HAS_REVERSE_WINDOW_SCORE_GAIN = True
+except ImportError:
+    _HAS_REVERSE_WINDOW_SCORE_GAIN = False
+
 
 # --- Numba reference implementations ---
 
@@ -44,6 +51,17 @@ def _reverse_window(order, start, length):
         tmp = order[start + i]
         order[start + i] = order[start + length - i - 1]
         order[start + length - i - 1] = tmp
+
+
+@njit
+def _reverse_window_score_gain(masks, order, start, length):
+    forward_score = _mask_delta_score(masks[order[start - 1]], masks[order[start]]) + _mask_delta_score(
+        masks[order[start + length - 1]], masks[order[start + length]]
+    )
+    reverse_score = _mask_delta_score(masks[order[start - 1]], masks[order[start + length - 1]]) + _mask_delta_score(
+        masks[order[start]], masks[order[start + length]]
+    )
+    return forward_score - reverse_score
 
 
 # --- Benchmarks ---
@@ -84,3 +102,23 @@ class BenchmarkReverseWindow:
     @skip_benchmark_if(not _HAS_REVERSE_WINDOW)
     def time_cpp(self, n_features):
         _cpp_reverse_window(self.order.copy(), 1, self.length)
+
+
+class BenchmarkReverseWindowScoreGain:
+    params = [4, 8, 13]
+    param_names = ["n_features"]
+
+    def setup(self, n_features):
+        n_masks = 2**n_features
+        rng = np.random.default_rng(0)
+        self.masks = rng.integers(0, 2, (n_masks, n_features)).astype(bool)
+        self.order = rng.permutation(n_masks).astype(np.int64)
+        self.length = n_masks // 2
+        _reverse_window_score_gain(self.masks, self.order, 1, self.length)  # force JIT
+
+    def time_numba(self, n_features):
+        _reverse_window_score_gain(self.masks, self.order, 1, self.length)
+
+    @skip_benchmark_if(not _HAS_REVERSE_WINDOW_SCORE_GAIN)
+    def time_cpp(self, n_features):
+        _cpp_reverse_window_score_gain(self.masks, self.order, 1, self.length)
