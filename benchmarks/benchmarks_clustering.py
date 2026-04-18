c363477635e874e5a1b0b9c8d2408e2ad855dbfd
Arun Thakur <bedupako12mas@gmail.com>
2026-04-18 19:52:56 +0530

    BENCH: add benchmark for pt_shuffle_rec

--- benchmarks/benchmarks_clustering.py
+++ benchmarks/benchmarks_clustering.py
@@ -43,6 +43,13 @@ try:
 except ImportError:
     _HAS_DELTA_MINIMIZATION_ORDER = False
 
+try:
+    from shap._cutils import pt_shuffle_rec as _cpp_pt_shuffle_rec
+
+    _HAS_PT_SHUFFLE_REC = True
+except ImportError:
+    _HAS_PT_SHUFFLE_REC = False
+
 
 # --- Numba reference implementations ---
 
@@ -69,6 +76,25 @@ def _delta_minimization_order(all_masks, max_swap_size=100, num_passes=2):
                 if _reverse_window_score_gain(all_masks, order, i, length) > 0:
                     _reverse_window(order, i, length)
     return order
+
+
+@njit
+def _pt_shuffle_rec(i, indexes, index_mask, partition_tree, M, pos):
+    if i < 0:
+        if index_mask[i + M]:
+            indexes[pos] = i + M
+            return pos + 1
+        else:
+            return pos
+    left = int(partition_tree[i, 0] - M)
+    right = int(partition_tree[i, 1] - M)
+    if np.random.randn() < 0:
+        pos = _pt_shuffle_rec(left, indexes, index_mask, partition_tree, M, pos)
+        pos = _pt_shuffle_rec(right, indexes, index_mask, partition_tree, M, pos)
+    else:
+        pos = _pt_shuffle_rec(right, indexes, index_mask, partition_tree, M, pos)
+        pos = _pt_shuffle_rec(left, indexes, index_mask, partition_tree, M, pos)
+    return pos
 
 
 @njit
@@ -158,3 +184,49 @@ class BenchmarkDeltaMinimizationOrder:
     @skip_benchmark_if(not _HAS_DELTA_MINIMIZATION_ORDER)
     def time_cpp(self, n_features):
         _cpp_delta_minimization_order(self.masks)
+
+
+class BenchmarkPtShuffleRec:
+    params = [4, 8, 13]
+    param_names = ["n_features"]
+
+    def setup(self, n_features):
+        from scipy.cluster.hierarchy import ward
+        from scipy.spatial.distance import pdist
+
+        rng = np.random.default_rng(0)
+        data = rng.random((n_features, n_features))
+        self.partition_tree = ward(pdist(data)).astype(np.float64)
+        self.M = n_features
+        self.index_mask = np.ones(n_features, dtype=bool)
+        self.indexes = np.zeros(n_features, dtype=np.int64)
+        # force JIT compilation
+        _pt_shuffle_rec(
+            self.partition_tree.shape[0] - 1,
+            self.indexes.copy(),
+            self.index_mask,
+            self.partition_tree,
+            self.M,
+            0,
+        )
+
+    def time_numba(self, n_features):
+        _pt_shuffle_rec(
+            self.partition_tree.shape[0] - 1,
+            self.indexes.copy(),
+            self.index_mask,
+            self.partition_tree,
+            self.M,
+            0,
+        )
+
+    @skip_benchmark_if(not _HAS_PT_SHUFFLE_REC)
+    def time_cpp(self, n_features):
+        _cpp_pt_shuffle_rec(
+            self.partition_tree.shape[0] - 1,
+            self.indexes.copy(),
+            self.index_mask,
+            self.partition_tree,
+            self.M,
+            0,
+        )
