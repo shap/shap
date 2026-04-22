3189885131a9d7c61620ca5871d0e6211188123a
Avichal shrivastava <avichalshrivastava2006@gmail.com>
2026-04-22 02:52:51 +0000

    Add tests for shap.utils.image (improve coverage)

--- tests/utils/test_image.py
+++ tests/utils/test_image.py
@@ -1,22 +1,37 @@
 import numpy as np
 import shap.utils.image as img
-import cv2
+import tempfile
 import os
 
 
 def test_resize_image_behavior():
     dummy = np.ones((10, 10, 3), dtype=np.uint8) * 255
-    path = "test_img.png"
-    cv2.imwrite(path, dummy)
 
-    result = img.resize_image(path, (5, 5))
+    with tempfile.NamedTemporaryFile(suffix=".png", delete=False) as tmp:
+        path = tmp.name
 
-    # check it returns tuple
-    assert isinstance(result, tuple)
+    try:
+        import cv2
+        cv2.imwrite(path, dummy)
 
-    resized = result[0]
+        result = img.resize_image(path, (5, 5))
 
-    # check output is numpy array
-    assert isinstance(resized, np.ndarray)
+        
+        assert isinstance(result, tuple)
 
-    os.remove(path)
+        resized = result[0]
+
+        
+        assert isinstance(resized, np.ndarray)
+        assert resized.ndim == 3
+        assert resized.shape[2] == 3   # RGB channels
+
+    finally:
+        os.remove(path)
+
+
+def test_resize_image_invalid_path():
+    import pytest
+
+    with pytest.raises(Exception):
+        img.resize_image("invalid_path.png", (5, 5))
