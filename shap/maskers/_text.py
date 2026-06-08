831b795a1a1ac602d7db6b827185c211cbc02699
rishu ray <rayrishu19@gmail.com>
2026-06-08 13:23:14 +0530

    FIX: Add None check for sep_token in Text masker loop condition

--- shap/maskers/_text.py
+++ shap/maskers/_text.py
@@ -130,7 +130,7 @@ class Text(Masker):
             sep_token = getattr_silent(self.tokenizer, "sep_token")
             for i, v in enumerate(mask):
                 # mask ignores separator tokens and keeps them unmasked
-                if v or sep_token == self._segments_s[i]:
+                if v or (sep_token is not None and sep_token == self._segments_s[i]):
                     out_parts.append(self._segments_s[i])
                     is_previous_appended_token_mask_token = False
                 else:
