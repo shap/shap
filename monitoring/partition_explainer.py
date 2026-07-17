4be0871191871b31b03deefb7a5cdc89f1655e71
Zhihao Dai <zhihao.dai@eng.ox.ac.uk>
2026-07-18 00:28:55 +0100

    Add image partition explainer benchmarks

--- monitoring/partition_explainer.py
+++ monitoring/partition_explainer.py
@@ -1,7 +1,17 @@
+from asv_runner.benchmarks.mark import skip_benchmark_if
 from xgboost import XGBClassifier
 
-from shap.datasets import adult
+from shap import Explanation
+from shap.datasets import adult, imagenet50
 from shap.explainers import PartitionExplainer
+from shap.maskers import Image
+
+try:
+    from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
+except ImportError:
+    TENSORFLOW_AVAILABLE = False
+else:
+    TENSORFLOW_AVAILABLE = True
 
 
 class PartitionSuite:
@@ -30,3 +40,29 @@ class PartitionSuite:
     def time_multi_output(self):
         ex = PartitionExplainer(self.model.predict_proba, self.X)
         _ = ex(self.X)
+
+
+class ImagePartitionSuite:
+    # Adapted from "Image Data Explanation Benchmarking: Image Multiclass Classification" notebook
+
+    def setup(self):
+        if TENSORFLOW_AVAILABLE:
+            self.model = ResNet50(weights="imagenet")
+        self.X, self.y = imagenet50()
+
+    def predict(self, x):
+        tmp = x.copy()
+        if len(tmp.shape) == 2:
+            tmp = tmp.reshape(tmp.shape[0], *self.X[0].shape)
+        preprocess_input(tmp)
+        return self.model(tmp)
+
+    def time_masker_init(self):
+        _ = Image("inpaint_telea", self.X[0].shape)
+
+    @skip_benchmark_if(not TENSORFLOW_AVAILABLE)
+    def time_multi_output(self):
+        # Skipping this test for now because it is very slow and adds a dependency on tensorflow
+        masker = Image("inpaint_telea", self.X[0].shape)
+        explainer = PartitionExplainer(self.predict, masker)
+        _ = explainer(self.X[1:3], max_evals=500, batch_size=50, outputs=Explanation.argsort.flip[:4])
